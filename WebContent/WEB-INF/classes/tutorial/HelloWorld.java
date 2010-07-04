6cae240d85352fdece94c7bf276cd8e1b5f55ef3
wiktor <wiktor@zanoccio.com>
2010-07-03 19:49:23 -0500

    Basic working struts2 webapp integrated with eclipse

---
+++ WebContent/WEB-INF/classes/tutorial/HelloWorld.java
@@ -0,0 +1,23 @@
+package tutorial;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloWorld extends ActionSupport {
+	public static final String MESSAGE = "Struts is up and running ...";
+
+	@Override
+	public String execute() throws Exception {
+		setMessage(MESSAGE);
+		return SUCCESS;
+	}
+
+	private String message;
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
