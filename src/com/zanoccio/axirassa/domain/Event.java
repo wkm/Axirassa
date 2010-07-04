3f1ccc763809b59687fc7d6ebef8b53d3636830c
wiktor <wiktor@zanoccio.com>
2010-07-04 15:30:42 -0500

    Working on hibernate integration

---
+++ src/com/zanoccio/axirassa/domain/Event.java
@@ -0,0 +1,38 @@
+package com.zanoccio.axirassa.domain;
+
+import java.util.Date;
+
+public class Event {
+
+	private Long id;
+	private String title;
+	private Date date;
+
+	public Event() {
+	}
+
+	public Long getID() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+}
