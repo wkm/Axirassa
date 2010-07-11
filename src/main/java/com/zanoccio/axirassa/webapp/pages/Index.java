0316af2faa77607f5827c36250b4a12a5f4dc3cf
wiktor <wiktor@zanoccio.com>
2010-07-11 00:07:19 -0500

    Tapestry layout

---
+++ src/main/java/com/zanoccio/axirassa/webapp/pages/Index.java
@@ -0,0 +1,14 @@
+package com.zanoccio.axirassa.webapp.pages;
+
+import java.util.Date;
+
+/**
+ * Start page of application axir.
+ */
+public class Index
+{
+	public Date getCurrentTime() 
+	{ 
+		return new Date(); 
+	}
+}
