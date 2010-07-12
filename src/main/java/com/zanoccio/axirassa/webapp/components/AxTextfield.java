2312ead443da48ef87ce00f944e9736d812106f6
wiktor <wiktor@zanoccio.com>
2010-07-11 21:20:43 -0500

    Getting rid of old AxInput component; show labels in AxTextfields

--- src/main/java/com/zanoccio/axirassa/webapp/components/AxTextfield.java
+++ src/main/java/com/zanoccio/axirassa/webapp/components/AxTextfield.java
@@ -1,20 +1,42 @@
 
 package com.zanoccio.axirassa.webapp.components;
 
+import org.apache.tapestry5.ComponentResources;
 import org.apache.tapestry5.annotations.Component;
+import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.annotations.SupportsInformalParameters;
 import org.apache.tapestry5.corelib.components.TextField;
+import org.apache.tapestry5.ioc.annotations.Inject;
 
 @SupportsInformalParameters
 public class AxTextfield {
-	@Component(publishParameters = "annotationProvider,clientId,disabled,label,nulls,translate,validate,value")
+	@Inject
+	private ComponentResources resources;
+
+	@Component(publishParameters = "annotationProvider,clientId,disabled,nulls,translate,validate,value")
 	private TextField txtfield;
+
+	@Property
+	private String name;
 
 	@Property
 	private String text;
 
-	// @Property
-	// private String label;
+	@Parameter
+	private String label;
+
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+
+	public String getLabel() {
+		if (label == null)
+			return resources.getId();
+		else
+			return label;
+	}
 
 }
