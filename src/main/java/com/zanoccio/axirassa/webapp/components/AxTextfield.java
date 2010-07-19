9f9d28af5c813bc5c68a33b2b59b83ad849cd3fa
wiktor <wiktor@zanoccio.com>
2010-07-19 14:45:08 -0500

    working on providing validation directly in a textfield component

--- src/main/java/com/zanoccio/axirassa/webapp/components/AxTextfield.java
+++ src/main/java/com/zanoccio/axirassa/webapp/components/AxTextfield.java
@@ -1,14 +1,54 @@
 
 package com.zanoccio.axirassa.webapp.components;
 
+import org.apache.tapestry5.Field;
+import org.apache.tapestry5.MarkupWriter;
+import org.apache.tapestry5.annotations.AfterRender;
+import org.apache.tapestry5.annotations.BeginRender;
 import org.apache.tapestry5.annotations.Component;
+import org.apache.tapestry5.annotations.Environmental;
 import org.apache.tapestry5.annotations.SupportsInformalParameters;
 import org.apache.tapestry5.corelib.components.TextField;
+import org.apache.tapestry5.dom.Element;
+import org.apache.tapestry5.services.Heartbeat;
 
 @SupportsInformalParameters
 public class AxTextField extends AxAbstractTextInput {
 	@Component(publishParameters = "annotationProvider,clientId,disabled,nulls,translate,validate,value")
 	private TextField txtfield;
+
+	@Environmental
+	private Heartbeat heartbeat;
+
+	private Element labelelement;
+
+	private String value;
+
+
+	@BeginRender
+	void beginRender(MarkupWriter writer) {
+		final Field txtfield = this.txtfield;
+		labelelement = writer.element("span", "class", "msg");
+
+		Runnable command = new Runnable() {
+			@Override
+			public void run() {
+				String fieldId = txtfield.getClientId();
+				labelelement.forceAttributes("id", fieldId + "-msg");
+			}
+		};
+
+		heartbeat.defer(command);
+
+		return;
+	}
+
+
+	@AfterRender
+	void afterRender(MarkupWriter writer) {
+		writer.write(value);
+		writer.end();
+	}
 
 
 	@Override
