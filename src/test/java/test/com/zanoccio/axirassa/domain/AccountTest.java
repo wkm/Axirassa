4fd870b85e6d8e0709be31d1ee19a549ed9f3590
wiktor <wiktor@zanoccio.com>
2010-07-22 22:39:12 -0500

    AccountTest file

---
+++ src/test/java/test/com/zanoccio/axirassa/domain/AccountTest.java
@@ -0,0 +1,15 @@
+
+package test.com.zanoccio.axirassa.domain;
+
+import org.junit.Test;
+
+import com.zanoccio.axirassa.domain.AccountModel;
+import com.zanoccio.axirassa.util.SchemaTest;
+
+public class AccountTest extends SchemaTest {
+	@Test
+	public void accounts() {
+		AccountModel account = new AccountModel();
+	}
+
+}
