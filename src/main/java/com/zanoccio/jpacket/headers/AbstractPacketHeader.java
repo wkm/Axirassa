5d6237bd044e293302ddb8000a3890b135e8e8b0
wiktor <wiktor@zanoccio.com>
2010-07-25 01:01:21 -0500

    Working on AbstractPacketHeader#construct

--- src/main/java/com/zanoccio/jpacket/headers/AbstractPacketHeader.java
+++ src/main/java/com/zanoccio/jpacket/headers/AbstractPacketHeader.java
@@ -1,12 +1,19 @@
 
 package com.zanoccio.jpacket.headers;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public abstract class AbstractPacketHeader implements PacketHeader {
 
 	@Override
 	public List<Byte> construct() {
-		return null;
+		Field[] fields = getClass().getFields();
+		PriorityQueue<Field> queue = new PriorityQueue<Field>();
+		
+		for(field : fields) {
+			
+		}
 	}
 }
