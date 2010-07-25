61b4d66b0482d90ce020154bc188afdc67379c1d
wiktor <wiktor@zanoccio.com>
2010-07-24 23:42:36 -0500

    Simple IP4Address tests

--- src/test/java/test/com/zanoccio/jpacket/TestIP4Address.java
+++ src/test/java/test/com/zanoccio/jpacket/TestIP4Address.java
@@ -1,5 +1,25 @@
+
 package test.com.zanoccio.jpacket;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+import com.zanoccio.jpacket.IP4Address;
 
 public class TestIP4Address {
 
+	@Test
+	public void construction() {
+		IP4Address ip4 = IP4Address.parse("127.0.0.1");
+		assertNotNull(ip4);
+		assertEquals("127.0.0.1", ip4.toString());
+
+		assertNull(IP4Address.parse("127.0.0"));
+		assertNull(IP4Address.parse("127.0.0.0.1"));
+		assertNull(IP4Address.parse("127.-1.0.1"));
+		assertNull(IP4Address.parse("127..."));
+	}
 }
