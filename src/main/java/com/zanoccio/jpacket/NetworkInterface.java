d6d9c4347973221cbd53f29620b35638722c8e13
wiktor <wiktor@zanoccio.com>
2010-07-25 01:00:59 -0500

    Very rough NetworkInteface object

--- src/main/java/com/zanoccio/jpacket/NetworkInterface.java
+++ src/main/java/com/zanoccio/jpacket/NetworkInterface.java
@@ -1,5 +1,54 @@
+
 package com.zanoccio.jpacket;
+
+import java.io.IOException;
+
+import org.jnetpcap.Pcap;
+import org.jnetpcap.PcapAddr;
+import org.jnetpcap.PcapIf;
+import org.jnetpcap.PcapSockAddr;
+
+import com.zanoccio.jpacket.exceptions.JPacketException;
+import com.zanoccio.jpacket.exceptions.NoIP4AddressException;
 
 public class NetworkInterface {
 
+	private final int snaplength = 1024;
+	private final int flags = Pcap.MODE_NON_BLOCKING;
+	private final int timeout = 10 * 1000;
+	private final StringBuilder errorbuffer;
+
+	private final PcapIf device;
+
+	private final IP4Address ipaddress;
+	private final MACAddress macaddress;
+
+	private Pcap pcap;
+
+
+	public NetworkInterface(PcapIf device) throws JPacketException, IOException {
+		this.device = device;
+		this.macaddress = new MACAddress(device.getHardwareAddress());
+
+		errorbuffer = new StringBuilder();
+
+		// find the IP4 address for this network interface
+		IP4Address ipaddress = null;
+		for (PcapAddr address : device.getAddresses()) {
+			if (address.getAddr().getFamily() == PcapSockAddr.AF_INET) {
+				ipaddress = new IP4Address(address.getAddr().getData());
+				break;
+			}
+		}
+
+		if (ipaddress == null)
+			throw new NoIP4AddressException(device);
+
+		this.ipaddress = ipaddress;
+	}
+
+
+	public void openLive() {
+		pcap = Pcap.openLive(device.getName(), snaplength, flags, timeout, errorbuffer);
+	}
 }
