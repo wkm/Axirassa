0fb26daf4c610495afedcfc6c7fcf428a021d7f1
wiktor <wiktor@zanoccio.com>
2010-07-26 00:46:28 -0500

    Unit test for ARPHeader

--- src/test/java/test/com/zanoccio/jpacket/TestARPHeader.java
+++ src/test/java/test/com/zanoccio/jpacket/TestARPHeader.java
@@ -3,19 +3,26 @@ package test.com.zanoccio.jpacket;
 
 import org.junit.Test;
 
+import com.zanoccio.jpacket.IP4Address;
+import com.zanoccio.jpacket.MACAddress;
+import com.zanoccio.jpacket.exceptions.JPacketException;
 import com.zanoccio.jpacket.headers.ARPHardwareType;
 import com.zanoccio.jpacket.headers.ARPHeader;
 import com.zanoccio.jpacket.headers.ARPProtocolType;
+import com.zanoccio.jpacket.mock.MockNetworkInterface;
 
 public class TestARPHeader {
 
 	@Test
-	public void construct() {
+	public void construct() throws JPacketException {
 		ARPHeader arp = new ARPHeader();
 
 		arp.setHardwareType(ARPHardwareType.ETHERNET);
 		arp.setProtocolType(ARPProtocolType.IP4);
+		arp.setTargetMAC(MACAddress.BROADCAST);
+		arp.setTargetIP(IP4Address.EMPTY);
 
-		System.out.println();
+		arp.associate(new MockNetworkInterface());
+		System.out.println(arp.construct());
 	}
 }
