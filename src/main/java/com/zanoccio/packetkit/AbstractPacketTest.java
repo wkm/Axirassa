c1d592f113629a212214f46b5d66ebbb4e886806
wiktor <wiktor@zanoccio.com>
2010-08-01 17:06:07 -0500

    Support for tests with their hexdumps in .properties files

---
+++ src/main/java/com/zanoccio/packetkit/AbstractPacketTest.java
@@ -0,0 +1,50 @@
+
+package com.zanoccio.packetkit;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Properties;
+
+public class AbstractPacketTest {
+	public static HashMap<Class<? extends AbstractPacketTest>, Properties> properties = new HashMap<Class<? extends AbstractPacketTest>, Properties>();
+
+
+	public String getProperties(String key) {
+		Properties prop = loadProperties(getClass());
+		return prop.getProperty(key);
+	}
+
+
+	private Properties loadProperties(Class<? extends AbstractPacketTest> klass) {
+		if (properties.get(klass) != null)
+			return properties.get(klass);
+
+		String fname = klass.getCanonicalName().replace('.', '\\') + ".properties";
+		System.out.println(fname);
+		InputStream istream = getClass().getClassLoader().getResourceAsStream(fname);
+		if (istream == null)
+			throw new ExceptionInInitializerError("Could not find properties for " + klass + " at " + fname);
+
+		Properties props = new Properties();
+		try {
+			props.load(istream);
+			properties.put(klass, props);
+			istream.close();
+
+			return props;
+		} catch (FileNotFoundException e) {
+			throw new ExceptionInInitializerError(e);
+		} catch (IOException e) {
+			throw new ExceptionInInitializerError(e);
+		} finally {
+			try {
+				istream.close();
+			} catch (IOException e) {
+				// ignore
+			}
+		}
+
+	}
+}
---
+++ src/test/java/test/com/zanoccio/packetkit/TestICMPHeader.java
@@ -0,0 +1,24 @@
+
+package test.com.zanoccio.packetkit;
+
+import static com.zanoccio.packetkit.PacketUtilities.assertPacketEquals;
+
+import org.junit.Test;
+
+import com.zanoccio.packetkit.AbstractPacketTest;
+import com.zanoccio.packetkit.exceptions.PacketKitException;
+import com.zanoccio.packetkit.headers.ICMPHeader;
+import com.zanoccio.packetkit.headers.ICMPType;
+
+public class TestICMPHeader extends AbstractPacketTest {
+	@Test
+	public void test() throws PacketKitException {
+		ICMPHeader icmp = new ICMPHeader();
+		icmp.setType(ICMPType.ECHO_REQUEST);
+		icmp.setCode(0);
+		icmp.setIdentifier(1);
+		icmp.setSequenceNumber(13);
+
+		assertPacketEquals(getProperties("IcmpEchoRequest"), icmp.construct());
+	}
+}
