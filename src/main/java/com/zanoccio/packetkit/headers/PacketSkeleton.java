0c9b04d580928338bfd596d85af540ebfa716ce7
wiktor <wiktor@zanoccio.com>
2010-08-11 23:35:28 -0500

    Working on supporting explicit offset/size for fragment slots.

--- src/main/java/com/zanoccio/packetkit/headers/PacketSkeleton.java
+++ src/main/java/com/zanoccio/packetkit/headers/PacketSkeleton.java
@@ -7,6 +7,7 @@ import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.PriorityQueue;
 
 import com.zanoccio.packetkit.IP4Address;
@@ -31,6 +32,11 @@ import com.zanoccio.packetkit.headers.annotations.StaticFragment;
  * 
  */
 public class PacketSkeleton {
+
+	public static final int DEFAULT_FRAGMENT_SLOT = 0;
+	public static final int DATA_FRAGMENT_SLOT = 100;
+	public static final int CHECKSUM_FRAGMENT_SLOT = 200;
+
 	public static HashMap<Class<? extends Object>, FragmentSlotType> VALIDPRIMITIVES;
 	static {
 		VALIDPRIMITIVES = new HashMap<Class<? extends Object>, FragmentSlotType>();
@@ -44,7 +50,7 @@ public class PacketSkeleton {
 	private Integer size;
 
 	private ArrayList<Field> fieldsfromnetworkinterface;
-	private PriorityQueue<FragmentSlot> queue;
+	private ArrayList<FragmentSlot> slotlist;
 
 
 	public PacketSkeleton(Class<? extends PacketHeader> klass) throws PacketKitException {
@@ -54,6 +60,8 @@ public class PacketSkeleton {
 
 	@SuppressWarnings("unchecked")
 	public void construct(Class<? extends PacketHeader> klass) throws PacketKitException {
+		int slotindex = DEFAULT_FRAGMENT_SLOT;
+
 		isfixedsize = true;
 
 		this.name = klass.getName();
@@ -62,8 +70,101 @@ public class PacketSkeleton {
 		Field[] fields = klass.getDeclaredFields();
 
 		HashSet<Integer> slots = new HashSet<Integer>();
-		queue = new PriorityQueue<FragmentSlot>();
+		PriorityQueue<FragmentSlot> queue = new PriorityQueue<FragmentSlot>();
+
 		fieldsfromnetworkinterface = new ArrayList<Field>();
+
+		//
+		// Extract all annotated fields and sort by slot
+		//
+		int offset = 0;
+		for (Field field : fields) {
+			int fragmentslot;
+			FragmentSlotType fragmenttype = null;
+			FragmentSlot fragment = new FragmentSlot();
+
+			// pull the StaticFragment annotation for this field
+			StaticFragment annotation = field.getAnnotation(StaticFragment.class);
+
+			// no StaticFragment annotation
+			if (annotation == null)
+				continue;
+
+			// check whether a slot was specified
+			if (annotation.slot() != StaticFragment.DEFAULT_SLOT)
+				// use it
+				fragmentslot = annotation.slot();
+			else
+				// otherwise, use the slot index
+				fragmentslot = slotindex;
+
+			// increment the slot index
+			slotindex++;
+
+			// check that the slot hasn't been taken
+			if (slots.contains(slotindex))
+				throw new SlotTakenException(field);
+
+			// reserve the slot
+			slots.add(slotindex);
+
+			// get the type of the field
+			Class<? extends Object> fieldtype = field.getType();
+
+			// check whether this field is autowired
+			if (field.isAnnotationPresent(FromNetworkInterface.class)) {
+				if (fieldtype == IP4Address.class)
+					fragmenttype = FragmentSlotType.IP4ADDRESS;
+				else if (fieldtype == MACAddress.class)
+					fragmenttype = FragmentSlotType.MACADDRESS;
+				else
+					throw new CannotPopulateFromNetworkInterfaceException(field, fieldtype);
+			}
+
+			// verify that the field's type is compatible with the framework
+			if (!VALIDPRIMITIVES.containsKey(fieldtype)) {
+				// if this type isn't being autowired
+				if (fragmenttype != null)
+					// and the type is being valid
+					if (isValidType(fieldtype))
+						fragmenttype = FragmentSlotType.PACKETFRAGMENT;
+			}
+
+			// ensure that the field is efficiently accessible
+			if (!isFieldAccessible(field))
+				throw new InvalidFieldException(field, "must be public or package scoped");
+
+			// compute the size of this fragment
+			if (annotation.size() == StaticFragment.DEFAULT_SIZE) {
+				// check whether the field has a fixed size defines
+				if (fieldtype.isAnnotationPresent(FixedSize.class)) {
+					FixedSize sizeanno = fieldtype.getAnnotation(FixedSize.class);
+					fragment.size = sizeanno.size();
+				} else
+					// the fragment is dynamic, and thus so is the packet
+					fragment.size = FixedSize.DYNAMIC;
+			} else {
+				fragment.size = annotation.size();
+			}
+
+			// set the offset for this fragment
+			fragment.offset = offset;
+			fragment.slot = slotindex;
+
+			// if this fragment had a size, increase the offset
+			if (fragment.size != FixedSize.DYNAMIC)
+				offset += fragment.size;
+
+			// TODO final checks on the fragment (positive size, etc.)
+
+			queue.add(fragment);
+		}
+
+		System.out.println("Queue: " + queue);
+
+		//
+		// Process each annotated field in slot-order
+		//
 
 		int packetsize = 0;
 		// examine each field to construct this packet's skeleton
@@ -185,8 +286,17 @@ public class PacketSkeleton {
 				throw new InvalidFieldException(field, e);
 			}
 
+			// construct the FragmentSlot
+			FragmentSlot fragslot = new FragmentSlot();
+			fragslot.type = slottype;
+			fragslot.slot = slot;
+			fragslot.size = fragmentsize;
+			fragslot.offset = packetsize;
+			fragslot.fixed = isfragmentfixed;
+			fragslot.constructor = constructor;
+
 			slots.add(Integer.valueOf(slot));
-			queue.add(new FragmentSlot(slottype, slot, field, fragmentsize, isfragmentfixed, constructor));
+			queue.add(fragslot);
 
 			if (fragmentsize > 0)
 				packetsize += fragmentsize;
@@ -199,6 +309,32 @@ public class PacketSkeleton {
 			if (!klass.isAnnotationPresent(DynamicSize.class))
 				throw new NotDeclareDynamicException(klass);
 		}
+	}
+
+
+	/**
+	 * Gives true if the given field is accessible (ie, either public or
+	 * package-accessible)
+	 */
+	private boolean isFieldAccessible(Field field) {
+		int modifiers = field.getModifiers();
+		return !(Modifier.isPrivate(modifiers) || Modifier.isProtected(modifiers));
+	}
+
+
+	/**
+	 * Gives true if the given class implements {@link PacketFragment}.
+	 */
+	private boolean isValidType(Class<? extends Object> fieldtype) {
+		Class<? extends Object>[] interfaces = fieldtype.getInterfaces();
+		boolean validtype = false;
+		for (Class<? extends Object> iface : interfaces)
+			if (iface == PacketFragment.class) {
+				validtype = true;
+				break;
+			}
+
+		return validtype;
 	}
 
 
@@ -215,7 +351,7 @@ public class PacketSkeleton {
 		sb.append("\tfixed: " + isFixedSize()).append('\n');
 		sb.append("\tsize: " + getSize()).append('\n');
 
-		for (FragmentSlot slot : queue) {
+		for (FragmentSlot slot : slotlist) {
 			sb.append(String.format("\t%d:%s  %d  %15s  %15s  %s\n", slot.slot, slot.fixed ? "fixed " : "dynamic",
 			                        slot.size, slot.type, slot.field.getName(), slot.field.getType().getCanonicalName()));
 		}
@@ -235,14 +371,16 @@ public class PacketSkeleton {
 	}
 
 
-	public PriorityQueue<FragmentSlot> getSlots() {
-		return queue;
+	public List<FragmentSlot> getSlots() {
+		return slotlist;
 	}
 }
 
 enum FragmentSlotType {
 	// general case
 	PACKETFRAGMENT,
+	CHECKSUM,
+	DATA,
 
 	// primitives
 	INT,
@@ -253,28 +391,57 @@ enum FragmentSlotType {
 	MACADDRESS;
 }
 
+/**
+ * Lightweight container for an individual fragment of a packet.
+ * 
+ * @author wiktor
+ */
 class FragmentSlot implements Comparable<FragmentSlot> {
 	public FragmentSlotType type;
 
+	/**
+	 * in what order this fragment should be filled relative to other fragments
+	 * in this packet
+	 */
 	public int slot;
+
+	/**
+	 * the field this fragment is stored in within a {@link PacketFragment}
+	 * container
+	 */
 	public Field field;
+
+	/**
+	 * the offset, in bytes, of this fragment from the start of a packet
+	 */
+	public int offset;
+
+	/**
+	 * the size, in bytes, of this fragment
+	 */
 	public int size;
+
+	/**
+	 * true if this fragment is fixed in size
+	 */
 	public boolean fixed;
+
+	/**
+	 * a reference to a Method with a signature of [byte[], int, int] ->
+	 * [PacketFragment]
+	 */
 	public Method constructor;
-
-
-	public FragmentSlot(FragmentSlotType type, int slot, Field field, int size, boolean fixed, Method constructor) {
-		this.slot = slot;
-		this.field = field;
-		this.size = size;
-		this.fixed = fixed;
-		this.type = type;
-		this.constructor = constructor;
-	}
 
 
 	@Override
 	public int compareTo(FragmentSlot other) {
 		return this.slot - other.slot;
+	}
+
+
+	@Override
+	public String toString() {
+		return "FragmentSlot(" + slot + "\n\tfield: " + field + "\n\toffset:" + offset + "\n\tsize:" + size
+		        + "\n\tfixed:" + fixed + "\n)";
 	}
 }
