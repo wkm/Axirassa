b38e7802b5588062faac1561f0974ddfddac12db
wiktor <wiktor@zanoccio.com>
2010-09-11 00:59:27 -0500

    Work on getting ICMPFrame tests to pass

--- src/test/java/test/com/zanoccio/packetkit/TestICMPFrame.java
+++ src/test/java/test/com/zanoccio/packetkit/TestICMPFrame.java
@@ -35,24 +35,23 @@ public class TestICMPFrame extends AbstractPacketTest {
 		ip.setHeaderLength(20);
 		ip.setServices(0);
 		ip.setTotalLength(60);
-
+		ip.setIdentification(15097);
 		ip.setFlags(0);
 		ip.setFragmentOffset(0);
 		ip.setTimeToLive(51);
 		ip.setProtocol(IPProtocol.ICMP);
 		ip.setSource(IP4Address.parse("74.125.95.99"));
 		ip.setDestination(IP4Address.parse("192.168.1.3"));
-
 		frame.addHeader(ip);
 
 		ICMPHeader icmp = new ICMPHeader();
-		icmp.setType(ICMPType.ECHO_REQUEST);
+		icmp.setType(ICMPType.ECHO_REPLY);
 		icmp.setCode(0);
 		icmp.setIdentifier(1);
 		icmp.setSequenceNumber(17);
 		icmp.setData(PacketUtilities.parseHexDump(getProperty("IcmpData")));
-
 		frame.addHeader(icmp);
+
 		frame.associate(new MockNetworkInterface());
 
 		PacketUtilities.assertPacketEquals(getProperty("IcmpFrame"), frame.construct());
