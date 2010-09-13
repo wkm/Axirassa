26069bdc24a1156689b5f680200ccd81004930b8
wiktor <wiktor@zanoccio.com>
2010-09-12 21:38:32 -0500

    Terrible name; small container for a byte[] array and a cursor; intended to make it easy to preserve the location of a cursor within an array.

---
+++ src/main/java/com/zanoccio/packetkit/ByteParser.java
@@ -0,0 +1,13 @@
+
+package com.zanoccio.packetkit;
+
+public class ByteParser {
+	public byte[] bytes;
+	public int cursor;
+
+
+	public ByteParser(byte[] bytes, int cursor) {
+		this.bytes = bytes;
+		this.cursor = cursor;
+	}
+}
