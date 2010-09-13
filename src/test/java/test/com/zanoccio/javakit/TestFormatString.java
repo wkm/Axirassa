d9c689d397dff354ca35614a1d200b34bb7fb1cc
wiktor <wiktor@zanoccio.com>
2010-09-12 22:22:37 -0500

    com.zanoccio.javakit --- a small library of useful, general java functions.

---
+++ src/test/java/test/com/zanoccio/javakit/TestFormatString.java
@@ -0,0 +1,31 @@
+
+package test.com.zanoccio.javakit;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.zanoccio.javakit.FormatString;
+
+public class TestFormatString {
+	@Test
+	public void cutoff() {
+		assertEquals("foo", FormatString.cutoff("foo", 3));
+		assertEquals("foo", FormatString.cutoff("foo", 4));
+		assertEquals("..", FormatString.cutoff("foo", 2));
+		assertEquals("f..", FormatString.cutoff("foobar", 3));
+
+		assertEquals("fo.", FormatString.cutoff("foobar", 3, "."));
+		assertEquals("...", FormatString.cutoff("foobar", 3, "..."));
+	}
+
+
+	@Test
+	public void center() {
+		assertEquals("  foo  ", FormatString.center("foo", 7));
+		assertEquals("..foo..", FormatString.center("foo", 7, '.'));
+		assertEquals(" foo  ", FormatString.center("foo", 6));
+		assertEquals("foobar", FormatString.center("foobar", 5));
+		assertEquals("foobar", FormatString.center("foobar", 6));
+	}
+}
