9902e1cd4f4091191f4afd90a0dc886d419e0d88
wiktor <wiktor@zanoccio.com>
2010-09-19 23:20:44 -0500

    Sample flotr plot

--- src/main/java/com/zanoccio/axirassa/webapp/pages/Sentinel.java
+++ src/main/java/com/zanoccio/axirassa/webapp/pages/Sentinel.java
@@ -1,9 +1,11 @@
 
 package com.zanoccio.axirassa.webapp.pages;
 
+import org.apache.tapestry5.annotations.Import;
+
 import com.zanoccio.axirassa.webapp.annotations.PublicPage;
 
 @PublicPage
+@Import(library = "${tapestry.scriptaculous}/dragdrop.js")
 public class Sentinel {
-
 }
