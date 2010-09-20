180fba3397305a51db9d86e791c60dd6c2f18a09
wiktor <wiktor@zanoccio.com>
2010-09-20 01:02:22 -0500

    Working on ajaxian plots

--- src/main/java/com/zanoccio/axirassa/webapp/pages/Sentinel.java
+++ src/main/java/com/zanoccio/axirassa/webapp/pages/Sentinel.java
@@ -2,10 +2,26 @@
 package com.zanoccio.axirassa.webapp.pages;
 
 import org.apache.tapestry5.annotations.Import;
+import org.apache.tapestry5.annotations.InjectComponent;
+import org.apache.tapestry5.annotations.Persist;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.corelib.components.Zone;
 
 import com.zanoccio.axirassa.webapp.annotations.PublicPage;
 
 @PublicPage
 @Import(library = "${tapestry.scriptaculous}/prototype.js")
 public class Sentinel {
+	@Property
+	@Persist
+	private int clickCount;
+
+	@InjectComponent
+	private Zone counterZone;
+
+
+	Object onActionFromClicker() {
+		clickCount++;
+		return counterZone.getBody();
+	}
 }
