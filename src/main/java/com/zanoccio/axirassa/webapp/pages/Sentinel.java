cf024286beab4d9041cc9f9a7158f7129f7a6361
wiktor <wiktor@zanoccio.com>
2010-09-19 23:27:09 -0500

    Depend on prototype directly instead of through dragdrop.js which we don't even use

--- src/main/java/com/zanoccio/axirassa/webapp/pages/Sentinel.java
+++ src/main/java/com/zanoccio/axirassa/webapp/pages/Sentinel.java
@@ -6,6 +6,6 @@ import org.apache.tapestry5.annotations.Import;
 import com.zanoccio.axirassa.webapp.annotations.PublicPage;
 
 @PublicPage
-@Import(library = "${tapestry.scriptaculous}/dragdrop.js")
+@Import(library = "${tapestry.scriptaculous}/prototype.js")
 public class Sentinel {
 }
