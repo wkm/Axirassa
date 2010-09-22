541f4a8f326496b5ce1b9246b1d4f4a40700d62e
wiktor <wiktor@zanoccio.com>
2010-09-22 00:55:07 -0500

    Mock service runner that repeatedly runs a service with a delay.

---
+++ src/main/java/com/zanoccio/axirassa/services/runners/RepeatingServiceRunner.java
@@ -0,0 +1,13 @@
+
+package com.zanoccio.axirassa.services.runners;
+
+import com.zanoccio.axirassa.services.Service;
+
+public class RepeatingServiceRunner {
+	/**
+	 * Creates a service runner that executes the given service repeatedly.
+	 */
+	public RepeatingServiceRunner(Service service, int milliseconds) {
+
+	}
+}
