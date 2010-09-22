e64c6a36e3d19a264059af4ed5f36553721006da
wiktor <wiktor@zanoccio.com>
2010-09-22 00:55:34 -0500

    Very crude SentinelService test; but it works!

---
+++ src/test/java/test/com/zanoccio/axirassa/services/TestSentinelService.java
@@ -0,0 +1,29 @@
+
+package test.com.zanoccio.axirassa.services;
+
+import org.hibernate.Session;
+import org.junit.Test;
+
+import com.zanoccio.axirassa.services.Service;
+import com.zanoccio.axirassa.services.sentinel.SentinelService;
+import com.zanoccio.axirassa.util.AbstractDomainTest;
+
+public class TestSentinelService {
+
+	public static Session session;
+
+
+	@Test
+	public void test() throws Exception {
+		AbstractDomainTest.hibernateSession();
+		session = AbstractDomainTest.session;
+
+		Service service = new SentinelService(session, 1);
+
+		for (int i = 0; i < 3; i++) {
+			System.out.println(i + "...");
+			service.execute();
+			Thread.sleep(1000);
+		}
+	}
+}
