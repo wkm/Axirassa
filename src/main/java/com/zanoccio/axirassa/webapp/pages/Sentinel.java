a0f076e05e847b50279f622aa4fe13d3525e0e4c
wiktor <wiktor@zanoccio.com>
2010-09-23 20:26:20 -0500

    Output actual CPU usage data from the database instead of randomly generated data.

--- src/main/java/com/zanoccio/axirassa/webapp/pages/Sentinel.java
+++ src/main/java/com/zanoccio/axirassa/webapp/pages/Sentinel.java
@@ -1,6 +1,8 @@
 
 package com.zanoccio.axirassa.webapp.pages;
 
+import java.math.BigDecimal;
+import java.sql.Timestamp;
 import java.util.List;
 
 import org.apache.tapestry5.ComponentResources;
@@ -12,14 +14,14 @@ import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
-import com.zanoccio.axirassa.util.HibernateUtil;
+import com.zanoccio.axirassa.util.HibernateTools;
 import com.zanoccio.axirassa.webapp.annotations.PublicPage;
 
 @PublicPage
 @Import(library = "${tapestry.scriptaculous}/prototype.js")
 public class Sentinel {
 
-	private final static String querysql = "SELECT date, SUM(user), SUM(system) FROM SentinelCPUStats WHERE Machine_ID = 1 GROUP BY date";
+	private final static String querysql = "SELECT date, SUM(user), SUM(system) FROM SentinelCPUStats WHERE Machine_ID = 1 GROUP BY date ORDER BY date DESC";
 
 	// @InjectComponent
 	// private Request request;
@@ -31,12 +33,13 @@ public class Sentinel {
 	private ComponentResources resources;
 
 
+	@SuppressWarnings("boxing")
 	Object onActionFromCpuupdate() {
-		// if (!request.isXHR())
-		// // cleanly handle non-JS
-		// return "Sentinel";
+		if (!request.isXHR())
+			// cleanly handle non-JS
+			return "Sentinel";
 
-		Session session = HibernateUtil.getSession();
+		Session session = HibernateTools.getSession();
 
 		// execute a search query
 		Transaction transaction = session.beginTransaction();
@@ -45,8 +48,16 @@ public class Sentinel {
 
 		JSONArray obj = new JSONArray();
 		for (Object[] row : result) {
-			obj.put(row);
+			Timestamp time = (Timestamp) row[0];
+			double user = 100 * ((BigDecimal) row[1]).doubleValue();
+			double system = 100 * ((BigDecimal) row[2]).doubleValue();
+
+			System.out.println("Current time: " + time);
+
+			obj.put(new JSONArray(time.getTime(), user, system));
 		}
+
+		session.close();
 
 		return obj;
 	}
