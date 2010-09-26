32e0835b45d95a9eadbba4001f19733a0eadbee8
wiktor <wiktor@zanoccio.com>
2010-09-25 19:25:47 -0500

    Set of utility functions from creating Tapestry 5 JSONLiteral objects from Java objects.

---
+++ src/main/java/com/zanoccio/axirassa/util/JSONConstructor.java
@@ -0,0 +1,58 @@
+
+package com.zanoccio.axirassa.util;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.apache.tapestry5.json.JSONLiteral;
+
+public class JSONConstructor {
+	public static final int BUFFER_SIZE = 1024 * 1024;
+
+
+	public static JSONLiteral generate(Double[][][] array) {
+		StringBuilder sb = new StringBuilder(BUFFER_SIZE);
+		sb.append('[');
+		for (int i = 0; i < array.length; i++) {
+			sb.append('[');
+			for (int j = 0; j < array[i].length; j++) {
+
+				sb.append('[');
+				for (int k = 0; k < array[i][j].length; k++) {
+					sb.append(array[i][j][k]);
+
+					if (k < array[i][j].length - 1)
+						sb.append(',');
+				}
+				sb.append(']');
+
+				if (j < array[i].length - 1)
+					sb.append(',');
+			}
+			sb.append(']');
+
+			if (i < array.length - 1)
+				sb.append(',');
+		}
+		sb.append(']');
+
+		return new JSONLiteral(sb.toString());
+	}
+
+
+	public static JSONLiteral generate(Collection<Long> data) {
+		StringBuilder sb = new StringBuilder(BUFFER_SIZE);
+		Iterator<Long> iter = data.iterator();
+
+		sb.append('[');
+		while (iter.hasNext()) {
+			sb.append(iter.next());
+
+			if (iter.hasNext())
+				sb.append(',');
+		}
+		sb.append(']');
+
+		return new JSONLiteral(sb.toString());
+	}
+}
