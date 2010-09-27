2b2df6f92094045c1f66c4e9bb2f84ccd67e5d8d
wiktor <wiktor@zanoccio.com>
2010-09-27 02:35:25 -0500

    Lot's of copy-pasta; but show disk space results.

--- src/main/java/com/zanoccio/axirassa/webapp/pages/Sentinel.java
+++ src/main/java/com/zanoccio/axirassa/webapp/pages/Sentinel.java
@@ -11,7 +11,6 @@ import java.util.TreeSet;
 
 import org.apache.tapestry5.annotations.Import;
 import org.apache.tapestry5.ioc.annotations.Inject;
-import org.apache.tapestry5.json.JSONObject;
 import org.apache.tapestry5.services.Request;
 import org.hibernate.SQLQuery;
 import org.hibernate.Session;
@@ -27,6 +26,7 @@ public class Sentinel {
 
 	private final static String cpusql = "SELECT cpu, date, user, system FROM SentinelCPUStats WHERE Machine_ID = 1 ORDER BY cpu ASC, date ASC";
 	private final static String memsql = "SELECT date, used, total FROM SentinelMemoryStats WHERE Machine_ID = 1 ORDER BY date ASC";
+	private final static String disksql = "SELECT disk, date, used, total FROM SentinelDiskUsageStats WHERE Machine_ID = 1 ORDER BY disk ASC, date ASC";
 
 	@Inject
 	private Request request;
@@ -44,7 +44,7 @@ public class Sentinel {
 		Session session = HibernateTools.getSession();
 
 		// execute the search query
-		session.beginTransaction();
+		// session.beginTransaction();
 		SQLQuery query = session.createSQLQuery(cpusql);
 		List<Object[]> data = query.list();
 		session.close();
@@ -125,7 +125,7 @@ public class Sentinel {
 
 		Session session = HibernateTools.getSession();
 
-		session.beginTransaction();
+		// session.beginTransaction();
 		SQLQuery query = session.createSQLQuery(memsql);
 		List<Object[]> result = query.list();
 		session.close();
@@ -161,9 +161,85 @@ public class Sentinel {
 
 
 	Object onActionFromDiskSpaceUpdate() {
-		if (!request.isXHR())
-			return "Sentinel";
+		Session session = HibernateTools.getSession();
 
-		return new JSONObject();
+		// execute the search query
+		// session.beginTransaction();
+		SQLQuery query = session.createSQLQuery(disksql);
+		List<Object[]> data = query.list();
+		session.close();
+
+		HashMap<String, Integer> diskindices = new HashMap<String, Integer>();
+
+		// compute the number of disks and accumulate all timestamps for which
+		// we have data
+		int diskcount = 0;
+		String currentdisk = null;
+
+		ArrayList<String> labels = new ArrayList<String>(diskcount);
+
+		TreeSet<Long> times = new TreeSet<Long>();
+		for (Object[] row : data) {
+			String disk = (String) row[0];
+			Timestamp time = (Timestamp) row[1];
+			times.add(time.getTime());
+
+			if (!disk.equals(currentdisk)) {
+				currentdisk = disk;
+				labels.add(disk);
+
+				// store the disk -> index mapping
+				diskindices.put(currentdisk, diskcount);
+				diskcount++;
+			}
+		}
+
+		// a buffer with a position for each <time, cpu> slot.
+		Double[][][] rawdata = new Double[diskcount][times.size()][1];
+
+		// fill out the rawdata buffer
+		currentdisk = null;
+		int diskindex = 0;
+		Iterator<Long> timeiter = null;
+		int timeindex = 0;
+		long maxmemory = 0;
+		long totalmemory = 0;
+
+		for (Object[] row : data) {
+			// pull data
+			String disk = (String) row[0];
+			Timestamp time = (Timestamp) row[1];
+			long realtime = time.getTime();
+			long used = Long.parseLong((String) row[2]);
+			long total = Long.parseLong((String) row[3]);
+
+			if (total > maxmemory)
+				maxmemory = total;
+
+			// are we at the next cpu?
+			if (!disk.equals(currentdisk)) {
+				totalmemory += maxmemory;
+				maxmemory = 0;
+				currentdisk = disk;
+				diskindex = diskindices.get(currentdisk);
+
+				timeiter = times.iterator();
+				timeindex = 0;
+			}
+
+			// skip over any missing times
+			while (timeiter.next() < realtime) {
+				rawdata[diskindex][timeindex][0] = null;
+				timeindex++;
+			}
+
+			rawdata[diskindex][timeindex][0] = (double) used;
+			timeindex++;
+		}
+
+		AxPlotData plotdata = new AxPlotData(diskcount, 1, labels, times, rawdata);
+		plotdata.setAggregatedMax(totalmemory);
+		plotdata.setYAxisLabelingFunction(AxPlotAxisLabelingFunction.DATA);
+		return plotdata.toJSON();
 	}
 }
