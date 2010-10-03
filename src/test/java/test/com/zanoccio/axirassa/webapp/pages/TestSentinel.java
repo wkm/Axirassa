bf098b7e51d566f64f7c795633c5ebac6066d482
wiktor <wiktor@zanoccio.com>
2010-10-03 15:55:22 -0500

    Simple benchmark for the Sentinel:cpuupdate action.

--- src/test/java/test/com/zanoccio/axirassa/webapp/pages/TestSentinel.java
+++ src/test/java/test/com/zanoccio/axirassa/webapp/pages/TestSentinel.java
@@ -1,13 +1,22 @@
 
 package test.com.zanoccio.axirassa.webapp.pages;
 
-import org.apache.tapestry5.test.PageTester;
 import org.junit.Test;
+
+import com.zanoccio.axirassa.util.HibernateTools;
+import com.zanoccio.axirassa.webapp.pages.Sentinel;
 
 public class TestSentinel {
 	@Test
 	public void testCPUUpdate() {
-		PageTester tester = new PageTester("com.zanoccio.axirassa.webapp", "", "src/main/java");
-		tester.renderPage("sentinel.cpuupdate");
+		HibernateTools.getSession().disconnect();
+
+		Sentinel sent = new Sentinel();
+		int i = 5;
+		do {
+			long start = System.currentTimeMillis();
+			sent.onActionFromCpuupdate();
+			System.out.println("time: " + (System.currentTimeMillis() - start));
+		} while (i-- > 0);
 	}
 }
