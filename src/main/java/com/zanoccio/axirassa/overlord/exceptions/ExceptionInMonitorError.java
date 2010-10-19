f1fea4d7902698679fe6976ae641323b4af6f05e
wiktor <wiktor@zanoccio.com>
2010-10-18 21:50:38 -0500

    Error thrown if an exception is raised during the evaluation of a monitor; this should only be the result of some systemic issue.

---
+++ src/main/java/com/zanoccio/axirassa/overlord/exceptions/ExceptionInMonitorError.java
@@ -0,0 +1,12 @@
+
+package com.zanoccio.axirassa.overlord.exceptions;
+
+public class ExceptionInMonitorError extends Error {
+	private static final long serialVersionUID = -6410122032654147761L;
+
+
+	public ExceptionInMonitorError(Exception e) {
+		super(e);
+	}
+
+}
