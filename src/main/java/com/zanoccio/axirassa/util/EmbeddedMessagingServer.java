477db3cba4de97411038c34e6a647cfa3cd2c4e9
wiktor <wiktor@zanoccio.com>
2010-10-26 22:51:57 -0500

    Force use of NIO instead of AIO for journaling in HornetQ.

--- src/main/java/com/zanoccio/axirassa/util/EmbeddedMessagingServer.java
+++ src/main/java/com/zanoccio/axirassa/util/EmbeddedMessagingServer.java
@@ -7,9 +7,10 @@ import org.hornetq.api.core.TransportConfiguration;
 import org.hornetq.core.config.Configuration;
 import org.hornetq.core.config.impl.ConfigurationImpl;
 import org.hornetq.core.remoting.impl.invm.InVMAcceptorFactory;
+import org.hornetq.core.remoting.impl.netty.NettyAcceptorFactory;
 import org.hornetq.core.server.HornetQServer;
 import org.hornetq.core.server.HornetQServers;
-import org.hornetq.integration.transports.netty.NettyAcceptorFactory;
+import org.hornetq.core.server.JournalType;
 
 /**
  * Starts HornetQ as an embedded messaging server.
@@ -25,6 +26,7 @@ public class EmbeddedMessagingServer {
 		transports.add(new TransportConfiguration(NettyAcceptorFactory.class.getName()));
 		transports.add(new TransportConfiguration(InVMAcceptorFactory.class.getName()));
 
+		config.setJournalType(JournalType.NIO);
 		config.setAcceptorConfigurations(transports);
 		config.setSecurityEnabled(false);
 
