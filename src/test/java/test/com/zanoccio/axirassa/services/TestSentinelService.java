24d7301ff76015a861850d6cb8c782257faa31d9
wiktor <wiktor@zanoccio.com>
2010-10-30 20:36:18 -0500

    Fix sentinel service test to use new #addAgent() mechanism

--- src/test/java/test/com/zanoccio/axirassa/services/TestSentinelService.java
+++ src/test/java/test/com/zanoccio/axirassa/services/TestSentinelService.java
@@ -4,7 +4,10 @@ package test.com.zanoccio.axirassa.services;
 import org.hibernate.Session;
 import org.junit.Test;
 
-import com.zanoccio.axirassa.services.Service;
+import com.zanoccio.axirassa.services.sentinel.CPUSentinelAgent;
+import com.zanoccio.axirassa.services.sentinel.DiskSentinelAgent;
+import com.zanoccio.axirassa.services.sentinel.MemorySentinelAgent;
+import com.zanoccio.axirassa.services.sentinel.NetworkSentinelAgent;
 import com.zanoccio.axirassa.services.sentinel.SentinelService;
 import com.zanoccio.axirassa.util.AbstractDomainTest;
 
@@ -18,7 +21,14 @@ public class TestSentinelService {
 		AbstractDomainTest.hibernateSession();
 		session = AbstractDomainTest.session;
 
-		Service service = new SentinelService(session, 1);
+		SentinelService service = new SentinelService(session, 1);
+
+		service.addAgent(CPUSentinelAgent.class);
+		service.addAgent(MemorySentinelAgent.class);
+		service.addAgent(DiskSentinelAgent.class);
+		service.addAgent(NetworkSentinelAgent.class);
+
+		service.initialize();
 
 		for (int i = 0; i < (60 * 8); i++) {
 			service.execute();
