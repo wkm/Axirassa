cb5dcd079c2c2a169a642d669edab7e69c396553
wiktor <wiktor@zanoccio.com>
2010-10-31 01:15:56 -0500

    Enable network and disk IO statistics plotting; there's huge amounts of near-repetition happening in this file, it needs to be broken up as with the agents into individual classes for each kind of data.

--- src/main/java/com/zanoccio/axirassa/webapp/pages/Sentinel.java
+++ src/main/java/com/zanoccio/axirassa/webapp/pages/Sentinel.java
@@ -2,6 +2,7 @@
 package com.zanoccio.axirassa.webapp.pages;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.sql.Timestamp;
 import java.util.List;
 
@@ -23,10 +24,15 @@ import com.zanoccio.javakit.ListUtilities;
 @Import(library = "${tapestry.scriptaculous}/prototype.js")
 public class Sentinel {
 
-	private final static String cpusql = "SELECT CPU, Date, User, System FROM SentinelCPUStats WHERE Machine_ID = 1 ORDER BY CPU ASC, Date ASC LIMIT 1000";
-	private final static String memsql = "SELECT Date, Used, Total FROM SentinelMemoryStats WHERE Machine_ID = 1 ORDER BY Date ASC LIMIT 1000";
-	private final static String disksql = "SELECT Disk, Date, Used, Total FROM SentinelDiskUsageStats WHERE Machine_ID = 1 ORDER BY Disk ASC, Date ASC LIMIT 1000";
-	private final static String networksql = "SELECT Device, Date, Send, Receive FROM SentinelNetworkStats WHERE Machine_ID = 1 ORDER BY Device ASC, Date ASC LIMIT 1000";
+	private final static String cpusql = "SELECT CPU, Date, User, System FROM SentinelCPUStats WHERE Machine_ID = 1 ORDER BY CPU ASC, Date ASC";
+
+	private final static String memsql = "SELECT Date, Used, Total FROM SentinelMemoryStats WHERE Machine_ID = 1 ORDER BY Date ASC";
+
+	private final static String disksql = "SELECT Disk, Date, Used, Total FROM SentinelDiskUsageStats WHERE Machine_ID = 1 ORDER BY Disk ASC, Date ASC";
+	private final static String diskiosql = "SELECT Disk, Date, `Read`, `Write` FROM SentinelDiskIOStats WHERE Machine_ID = 1 ORDER BY Disk ASC, DATE ASC";
+
+	private final static String networksql = "SELECT Device, Date, Send, Receive FROM SentinelNetworkStats WHERE Machine_ID = 1 ORDER BY Device ASC, Date ASC";
+	private final static String networkiosql = "SELECT Device, Date, Send, Receive FROM SentinelNetworkIOStats WHERE Machine_ID = 1 ORDER BY Device ASC, Date ASC";
 
 	@Inject
 	private Request request;
@@ -120,8 +126,8 @@ public class Sentinel {
 		int i = 0;
 		for (Object[] row : result) {
 			Timestamp time = (Timestamp) row[0];
-			long used = Long.parseLong((String) row[1]);
-			long total = Long.parseLong((String) row[2]);
+			long used = ((BigInteger) row[1]).longValue();
+			long total = ((BigInteger) row[2]).longValue();
 
 			if (total > maxmemory)
 				maxmemory = total;
@@ -178,8 +184,8 @@ public class Sentinel {
 				String disk = (String) row[0];
 				Timestamp time = (Timestamp) row[1];
 				long realtime = time.getTime();
-				long used = Long.parseLong((String) row[2]);
-				long total = Long.parseLong((String) row[3]);
+				long used = ((BigInteger) row[2]).longValue();
+				long total = ((BigInteger) row[3]).longValue();
 
 				if (currentdataset.getLabel() == null)
 					currentdataset.setLabel("Disk - " + disk);
@@ -210,6 +216,57 @@ public class Sentinel {
 	}
 
 
+	public Object onActionFromDiskIOUpdate() {
+		Session session = HibernateTools.getSession();
+
+		SQLQuery query = session.createSQLQuery(diskiosql);
+		List<Object[]> data = query.list();
+		session.close();
+
+		List<List<Object[]>> iodata = ListUtilities.partition(data, new ListUtilities.ListPartitioner<Object[]>() {
+			@Override
+			public boolean partition(Object[] previous, Object[] current) {
+				return (!previous[0].equals(current[0]));
+			}
+		});
+
+		// build per-disk datasets
+		AxPlotDataPackage datapackage = new AxPlotDataPackage();
+		for (List<Object[]> dataset : iodata) {
+			int index = 0;
+			double[] times = new double[dataset.size()];
+			double[][] rawdata = new double[dataset.size()][2];
+
+			AxPlotDataSet currentdataset = new AxPlotDataSet();
+			for (Object[] row : dataset) {
+				String iface = (String) row[0];
+				Timestamp time = (Timestamp) row[1];
+				long realtime = time.getTime();
+				double readrate = (Float) row[2];
+				double writerate = (Float) row[3];
+
+				if (currentdataset.getLabel() == null)
+					currentdataset.setLabel(iface);
+
+				times[index] = realtime;
+				rawdata[index][0] = readrate;
+				rawdata[index][1] = writerate;
+
+				index++;
+			}
+
+			currentdataset.setData(times, rawdata);
+			currentdataset.setYPlotRange(0.0, null);
+			datapackage.addDataSet(currentdataset);
+		}
+
+		datapackage.setLabelDataSets(true);
+		datapackage.setYAxisLabelingFunction(AxPlotAxisLabelingFunction.DATA);
+
+		return datapackage.toJSON();
+	}
+
+
 	public Object onActionFromNetworkUpdate() {
 		Session session = HibernateTools.getSession();
 
@@ -227,9 +284,8 @@ public class Sentinel {
 
 		// build datasets per network interface
 		AxPlotDataPackage datapackage = new AxPlotDataPackage();
-		int index = 0;
 		for (List<Object[]> dataset : networkdata) {
-			index = 0;
+			int index = 0;
 			double[] times = new double[dataset.size()];
 			double[][] rawdata = new double[dataset.size()][2];
 
@@ -239,8 +295,8 @@ public class Sentinel {
 				String iface = (String) row[0];
 				Timestamp time = (Timestamp) row[1];
 				long realtime = time.getTime();
-				long sent = Long.parseLong((String) row[2]);
-				long received = Long.parseLong((String) row[3]);
+				long sent = ((BigInteger) row[2]).longValue();
+				long received = ((BigInteger) row[3]).longValue();
 
 				if (currentdataset.getLabel() == null)
 					currentdataset.setLabel(iface);
@@ -248,6 +304,57 @@ public class Sentinel {
 				times[index] = realtime;
 				rawdata[index][0] = sent;
 				rawdata[index][1] = received;
+
+				index++;
+			}
+
+			currentdataset.setData(times, rawdata);
+			currentdataset.setYPlotRange(0.0, null);
+			datapackage.addDataSet(currentdataset);
+		}
+
+		datapackage.setLabelDataSets(true);
+		datapackage.setYAxisLabelingFunction(AxPlotAxisLabelingFunction.DATA);
+
+		return datapackage.toJSON();
+	}
+
+
+	public Object onActionFromNetworkIOUpdate() {
+		Session session = HibernateTools.getSession();
+
+		SQLQuery query = session.createSQLQuery(networkiosql);
+		List<Object[]> data = query.list();
+		session.close();
+
+		List<List<Object[]>> iodata = ListUtilities.partition(data, new ListUtilities.ListPartitioner<Object[]>() {
+			@Override
+			public boolean partition(Object[] previous, Object[] current) {
+				return (!previous[0].equals(current[0]));
+			}
+		});
+
+		// build per network interface
+		AxPlotDataPackage datapackage = new AxPlotDataPackage();
+		for (List<Object[]> dataset : iodata) {
+			int index = 0;
+			double[] times = new double[dataset.size()];
+			double[][] rawdata = new double[dataset.size()][2];
+
+			AxPlotDataSet currentdataset = new AxPlotDataSet();
+			for (Object[] row : dataset) {
+				String iface = (String) row[0];
+				Timestamp time = (Timestamp) row[1];
+				long realtime = time.getTime();
+				double rxrate = ((BigInteger) row[2]).longValue();
+				double txrate = ((BigInteger) row[3]).longValue();
+
+				if (currentdataset.getLabel() == null)
+					currentdataset.setLabel(iface);
+
+				times[index] = realtime;
+				rawdata[index][0] = rxrate;
+				rawdata[index][1] = txrate;
 
 				index++;
 			}
