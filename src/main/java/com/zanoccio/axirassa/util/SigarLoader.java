12f236eb241f876b78e4d0b5a27e6168c7f24dec
wiktor <devnull@localhost>
2010-11-06 23:48:07 -0500

    Load platform appropriate Sigar libraries

--- src/main/java/com/zanoccio/axirassa/util/SigarLoader.java
+++ src/main/java/com/zanoccio/axirassa/util/SigarLoader.java
@@ -3,6 +3,10 @@ package com.zanoccio.axirassa.util;
 
 import java.io.IOException;
 
+import com.zanoccio.axirassa.overlord.exceptions.OverlordException;
+import com.zanoccio.axirassa.overlord.os.OverlordSystemSupport;
+import com.zanoccio.axirassa.overlord.os.AbstractOverlordSystemSupport;
+
 /**
  * Similar to {@link PcapLoader} but for Sigar.
  * 
@@ -26,13 +30,28 @@ public class SigarLoader {
 			loader.loadLibrary();
 		} catch (IOException e) {
 			throw new ExceptionInInitializerError(e);
+		} catch (OverlordException e) {
+			throw new ExceptionInInitializerError(e);
 		}
 
 		loaded = true;
 	}
 
 
-	public void loadLibrary() throws IOException {
-		JavaLibraryPath.addFile("/sigar-x86-winnt.dll");
+	public void loadLibrary() throws IOException, OverlordException {
+		OverlordSystemSupport systemsupport = AbstractOverlordSystemSupport.getSystemSupport();
+		
+		switch(systemsupport.getSystem()) {
+		case WINDOWS:
+			JavaLibraryPath.addFile("/sigar-x86-winnt.dll");
+			JavaLibraryPath.addFile("/sigar-amd64-winnt.dll");
+			break;
+			
+		case LINUX:
+			JavaLibraryPath.addFile("/libsigar-amd64-linux.so");
+			JavaLibraryPath.addFile("/libsigar-x86-linux.so");
+			JavaLibraryPath.addFile("/libsigar-ia64-linux.so");
+			break;
+		}
 	}
 }
