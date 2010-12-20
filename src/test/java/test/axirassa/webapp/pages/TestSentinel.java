a237713bdba803045589fdedd330b6988498f27d
wiktor@zanoccio.com <wiktor@zanoccio.com>
2010-12-20 00:35:51 -0700

    Update test to be compatible with a time offset parameter for updates

--- src/test/java/test/axirassa/webapp/pages/TestSentinel.java
+++ src/test/java/test/axirassa/webapp/pages/TestSentinel.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import axirassa.util.HibernateTools;
 import axirassa.webapp.pages.Sentinel;
 
-
 public class TestSentinel {
 	@Test
 	public void testCPUUpdate() {
@@ -16,7 +15,7 @@ public class TestSentinel {
 		int i = 5;
 		do {
 			long start = System.currentTimeMillis();
-			sent.onActionFromCpuupdate();
+			sent.onActionFromCpuupdate("24h");
 			System.out.println("time: " + (System.currentTimeMillis() - start));
 		} while (i-- > 0);
 	}
