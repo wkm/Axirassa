a4a5213a2d615120d61e08da2e8e5ebffdd30b70
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-01-04 14:29:40 -0700

    Move Register page from top-level to User/Register

---
+++ src/main/java/axirassa/webapp/pages/user/Register.java
@@ -0,0 +1,34 @@
+
+package axirassa.webapp.pages.user;
+
+import org.apache.tapestry5.annotations.Component;
+import org.apache.tapestry5.annotations.Persist;
+import org.apache.tapestry5.annotations.Property;
+
+import axirassa.webapp.components.AxForm;
+
+public class Register {
+	@Persist
+	@Property
+	private String email;
+
+	@Persist
+	@Property
+	private String confirmemail;
+
+	@Property
+	private String password;
+
+	@Property
+	private String confirmpassword;
+
+	@Component
+	private AxForm registerForm;
+
+
+	String onSuccess() {
+		System.out.println("Processing form.");
+		System.out.println("Email: " + email);
+		return "Index";
+	}
+}
