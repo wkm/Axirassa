f716f3aab8aa4e6230b78e24563dce20a8a8fab2
wiktor <wiktor@zanoccio.com>
2011-01-04 19:46:49 -0700

    Add support for labeling the AxSubmit button.

--- src/main/java/axirassa/webapp/components/AxSubmit.java
+++ src/main/java/axirassa/webapp/components/AxSubmit.java
@@ -2,9 +2,18 @@
 package axirassa.webapp.components;
 
 import org.apache.tapestry5.annotations.Component;
+import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.corelib.components.Submit;
 
 public class AxSubmit {
 	@Component(publishParameters = "context,defer,disabled,event,image")
 	private Submit submit;
+
+	@Parameter(defaultPrefix = "literal")
+	private String label;
+
+
+	public String getLabel() {
+		return label;
+	}
 }
