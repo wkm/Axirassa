f41aff0d23b517ed745d2c6b92527d535217de4e
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-01-08 21:21:14 -0700

    An enumeration of possibilities for the frequency of pingers.

---
+++ src/main/java/axirassa/domain/PingerFrequency.java
@@ -0,0 +1,31 @@
+
+package axirassa.domain;
+
+
+public enum PingerFrequency {
+	MINUTE(1),
+	MINUTES_5(5),
+	MINUTES_10(10),
+	MINUTES_15(15),
+	MINUTES_30(30),
+	MINUTES_60(60);
+	private int interval;
+
+
+	PingerFrequency(int interval) {
+		this.interval = interval;
+	}
+
+
+	public int getInterval() {
+		return interval;
+	}
+
+
+	public String getLabel() {
+		if (interval == 1)
+			return "minute";
+		else
+			return interval + " minutes";
+	}
+}
