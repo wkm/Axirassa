20194a86ca8b51ec893557a1d989dc3690a25ddd
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-01-09 20:37:25 -0700

    Add monitor type checkboxes (HTTP; HTTPS; ICMP), remove pointless PingerFrequencyValueEncoder, add #onSuccess method for actually executing the form.

--- src/main/java/axirassa/webapp/pages/monitor/CreateMonitor.java
+++ src/main/java/axirassa/webapp/pages/monitor/CreateMonitor.java
@@ -4,17 +4,27 @@ package axirassa.webapp.pages.monitor;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.tapestry5.ValueEncoder;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.util.EnumValueEncoder;
 
 import axirassa.domain.PingerFrequency;
-import axirassa.webapp.utilities.PingerFrequencyValueEncoder;
 
 public class CreateMonitor {
 	@Property
-	private final PingerFrequencyValueEncoder frequencyEncoder = new PingerFrequencyValueEncoder();
+	private final ValueEncoder<PingerFrequency> frequencyEncoder = new EnumValueEncoder(PingerFrequency.class);
 
 	@Property
 	private String url;
+
+	@Property
+	private boolean httpMonitor;
+
+	@Property
+	private boolean httpsMonitor;
+
+	@Property
+	private boolean icmpMonitor;
 
 	@Property
 	private PingerFrequency monitorFrequency;
@@ -22,6 +32,14 @@ public class CreateMonitor {
 	@Property
 	private final List<PingerFrequency> frequencies = Arrays.asList(PingerFrequency.values());
 
-	@Property
-	private PingerFrequency frequencyValue;
+
+	public String onSuccess() {
+		System.out.println("HTTP: " + httpMonitor);
+		System.out.println("HTTPS: " + httpsMonitor);
+		System.out.println("ICMP: " + icmpMonitor);
+		System.out.println("FREQ: " + monitorFrequency);
+		System.out.println("URL: " + url);
+
+		return "Index"; // TODO should forward to summary page for that monitor
+	}
 }
