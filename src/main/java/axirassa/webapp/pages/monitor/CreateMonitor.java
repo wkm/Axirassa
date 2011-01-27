9ad7bddf3a3b24fadfad018e46a09acdc1d07528
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-01-27 16:01:51 -0700

    Work on persisting monitors

--- src/main/java/axirassa/webapp/pages/monitor/CreateMonitor.java
+++ src/main/java/axirassa/webapp/pages/monitor/CreateMonitor.java
@@ -1,18 +1,31 @@
 
 package axirassa.webapp.pages.monitor;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.apache.shiro.authz.annotation.RequiresUser;
 import org.apache.tapestry5.ValueEncoder;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.util.EnumValueEncoder;
+import org.hibernate.Session;
+import org.tynamo.security.services.SecurityService;
 
+import axirassa.domain.MonitorType;
+import axirassa.domain.MonitorTypeModel;
 import axirassa.domain.PingerFrequency;
+import axirassa.domain.PingerModel;
 
 @RequiresUser
 public class CreateMonitor {
+	@Inject
+	private Session session;
+
+	@Inject
+	private SecurityService security;
+
 	@Property
 	private final ValueEncoder<PingerFrequency> frequencyEncoder = new EnumValueEncoder(PingerFrequency.class);
 
@@ -36,12 +49,26 @@ public class CreateMonitor {
 
 
 	public String onSuccess() {
-		System.out.println("HTTP: " + httpMonitor);
-		System.out.println("HTTPS: " + httpsMonitor);
-		System.out.println("ICMP: " + icmpMonitor);
-		System.out.println("FREQ: " + monitorFrequency);
-		System.out.println("URL: " + url);
+		ArrayList<Object> entities = new ArrayList<Object>();
 
-		return "Index"; // TODO should forward to summary page for that monitor
+		// save the pinger
+		PingerModel pinger = new PingerModel();
+		pinger.setUrl(url);
+		pinger.setFrequency(monitorFrequency);
+		entities.add(pinger);
+
+		if (httpMonitor)
+			entities.add(new MonitorTypeModel(pinger, MonitorType.HTTP));
+		if (httpsMonitor)
+			entities.add(new MonitorTypeModel(pinger, MonitorType.HTTPS));
+		if (icmpMonitor)
+			entities.add(new MonitorTypeModel(pinger, MonitorType.ICMP_PING));
+
+		session.beginTransaction();
+		for (Object entity : entities)
+			session.save(entity);
+		session.getTransaction().commit();
+
+		return "Monitor/List";
 	}
 }
