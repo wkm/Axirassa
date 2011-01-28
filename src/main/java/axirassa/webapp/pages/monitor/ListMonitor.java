69f401ad46f51d9a0c886044fae60b06034c5054
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-01-28 10:57:42 -0700

    Working monitor listing page, adjust CSS to support tables

--- src/main/java/axirassa/webapp/pages/monitor/ListMonitor.java
+++ src/main/java/axirassa/webapp/pages/monitor/ListMonitor.java
@@ -1,9 +1,43 @@
 
 package axirassa.webapp.pages.monitor;
 
+import java.util.List;
+
 import org.apache.shiro.authz.annotation.RequiresUser;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.tynamo.security.services.SecurityService;
+
+import axirassa.domain.MonitorTypeModel;
+import axirassa.domain.PingerModel;
+import axirassa.domain.UserModel;
 
 @RequiresUser
 public class ListMonitor {
+	@Inject
+	private Session session;
 
+	@Inject
+	private SecurityService security;
+
+	@Property
+	private List<PingerModel> pingers;
+
+	@Property
+	private PingerModel pinger;
+
+	@Property
+	private MonitorTypeModel type;
+
+
+	public void onActivate() {
+		UserModel user = UserModel.getUserByEmail(session, (String) security.getSubject().getPrincipal());
+
+		Query query = session.createQuery("from PingerModel where user=:user");
+		query.setEntity("user", user);
+
+		pingers = query.list();
+	}
 }
