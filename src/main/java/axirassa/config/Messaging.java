de4ec35dc9dbf6a6fb9bb3a23d7b4bcb1c1e43bc
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-01-30 16:45:51 -0700

    Probably a bad idea, but create a class with messaging constants. (like queue names)

---
+++ src/main/java/axirassa/config/Messaging.java
@@ -0,0 +1,12 @@
+
+package axirassa.config;
+
+/**
+ * A namespacing class containing queue names.
+ * 
+ * @author wiktor
+ */
+public class Messaging {
+	public static String PINGER_REQUEST_QUEUE = "Pinger.Request";
+	public static String PINGER_RESPONSE_QUEUE = "Pinger.Response";
+}
