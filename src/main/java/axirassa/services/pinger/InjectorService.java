f0e4b17c913ee79af908b35408f89e6ad2b3d95e
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-01-30 16:47:57 -0700

    Working injector service, instrumented with some basic benchmarking. Performance appears to be ~1800 messages in 216ms. (and 60k messages in ~5 sec)

--- src/main/java/axirassa/services/pinger/InjectorService.java
+++ src/main/java/axirassa/services/pinger/InjectorService.java
@@ -1,11 +1,21 @@
 
 package axirassa.services.pinger;
 
+import java.util.List;
+
 import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hornetq.api.core.client.ClientMessage;
+import org.hornetq.api.core.client.ClientProducer;
 import org.hornetq.api.core.client.ClientSession;
 
+import axirassa.config.Messaging;
+import axirassa.messaging.PingerRequestMessage;
+import axirassa.model.PingerModel;
 import axirassa.services.Service;
+import axirassa.util.EmbeddedMessagingServer;
+import axirassa.util.HibernateTools;
+import axirassa.util.MessagingTools;
 
 /**
  * The InjectorService executes every minute, creating a message for each
@@ -27,7 +37,40 @@ public class InjectorService implements Service {
 
 	@Override
 	public void execute() throws Exception {
-		Query query = databaseSession.createQuery("from PingerModel where interval=:interval");
+		ClientProducer producer = messagingSession.createProducer(Messaging.PINGER_REQUEST_QUEUE);
 
+		Query query = databaseSession.createQuery("from PingerModel where frequency=:frequency");
+		query.setInteger("frequency", 3600);
+
+		List<PingerModel> pingers = query.list();
+		long start = System.currentTimeMillis();
+		int msgcount = 0;
+		for (int i = 0; i < 300; i++) {
+			for (PingerModel pinger : pingers) {
+				ClientMessage message = messagingSession.createMessage(false);
+				message.getBodyBuffer().writeBytes(new PingerRequestMessage(pinger).toBytes());
+				producer.send(message);
+
+				msgcount++;
+			}
+		}
+		System.out.println("Sent " + msgcount + " messages in: " + (System.currentTimeMillis() - start) + "ms");
+
+		producer.close();
+	}
+
+
+	public static void main(String[] args) throws Exception {
+		EmbeddedMessagingServer.start();
+
+		ClientSession msgsession = MessagingTools.getEmbeddedSession();
+		Session dbsession = HibernateTools.getSession();
+
+		Service service = new InjectorService(msgsession, dbsession);
+
+		System.out.println("Executing injector");
+		service.execute();
+
+		return;
 	}
 }
