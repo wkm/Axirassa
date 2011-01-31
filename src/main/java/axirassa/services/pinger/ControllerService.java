fa2c8433cb92e63119a1b3fd349d4e9b85d42d7d
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-01-30 19:00:22 -0700

    Move message-consuming code from ControllerService into PingerService.

--- src/main/java/axirassa/services/pinger/ControllerService.java
+++ src/main/java/axirassa/services/pinger/ControllerService.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
-import org.hornetq.api.core.client.ClientConsumer;
 import org.hornetq.api.core.client.ClientMessage;
 import org.hornetq.api.core.client.ClientProducer;
 import org.hornetq.api.core.client.ClientSession;
@@ -14,11 +13,6 @@ import axirassa.config.Messaging;
 import axirassa.messaging.PingerRequestMessage;
 import axirassa.model.PingerModel;
 import axirassa.services.Service;
-import axirassa.util.AutoSerializingObject;
-import axirassa.util.EmbeddedMessagingServer;
-import axirassa.util.HibernateTools;
-import axirassa.util.MessagingTools;
-import axirassa.util.Meta;
 
 /**
  * The ControllerService executes every minute, creating a message for each
@@ -53,44 +47,5 @@ public class ControllerService implements Service {
 		}
 
 		producer.close();
-
-		// we have to start before reading messages
-		messagingSession.start();
-
-		ClientConsumer consumer = messagingSession.createConsumer(Messaging.PINGER_REQUEST_QUEUE);
-		while (true) {
-			ClientMessage message = consumer.receiveImmediate();
-			if (message == null) {
-				System.out.println("No more messages.");
-				break;
-			}
-
-			byte[] buffer = new byte[message.getBodyBuffer().readableBytes()];
-			message.getBodyBuffer().readBytes(buffer);
-
-			Object rawobject = AutoSerializingObject.fromBytes(buffer);
-			if (rawobject instanceof PingerRequestMessage)
-				Meta.inspect(rawobject);
-			else
-				System.err.println("WTF IS THIS: " + rawobject);
-		}
-
-		messagingSession.stop();
-	}
-
-
-	public static void main(String[] args) throws Exception {
-		EmbeddedMessagingServer.start();
-
-		ClientSession msgsession = MessagingTools.getEmbeddedSession();
-		Session dbsession = HibernateTools.getSession();
-
-		Service service = new ControllerService(msgsession, dbsession);
-
-		System.out.println("Executing injector");
-		service.execute();
-		System.out.println("Finished executing");
-
-		return;
 	}
 }
