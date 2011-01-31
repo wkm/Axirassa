98299d40135d14c6dfe463900b54a00d18c5fcd7
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-01-31 03:52:02 -0700

    Use InstrumentedHttpClient instead of HttpClient

--- src/main/java/axirassa/services/pinger/HTTPPinger.java
+++ src/main/java/axirassa/services/pinger/HTTPPinger.java
@@ -2,64 +2,41 @@
 package axirassa.services.pinger;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
 
 import axirassa.messaging.PingerResponseMessage;
+import axirassa.services.exceptions.PingerServiceException;
 
 public class HTTPPinger {
 
-	private final DefaultHttpClient client;
+	private final InstrumentedHttpClient client;
 	private final String url;
-	private long starttick;
-	private long responsetick;
-	private long finishtick;
 
 
 	public HTTPPinger(String url) {
-		client = new DefaultHttpClient();
+		client = new InstrumentedHttpClient();
 		this.url = url;
 	}
 
 
-	public PingerResponseMessage ping() throws ClientProtocolException, IOException {
+	public PingerResponseMessage ping() throws ClientProtocolException, IOException, PingerServiceException {
 		HttpGet get = new HttpGet(url);
-		HttpResponse response = client.execute(get);
-		HttpEntity entity = response.getEntity();
+		client.executeWithInstrumentation(get);
 
-		if (entity != null) {
-			StatusLine line = response.getStatusLine();
-			Header header = entity.getContentType();
-
-			InputStream input = entity.getContent();
-			InputStreamReader reader = new InputStreamReader(input);
-			StringBuilder sb = new StringBuilder();
-
-			char[] buffer = new char[4096];
-			int length;
-			while ((length = reader.read(buffer)) != -1)
-				sb.append(buffer, 0, length);
-			finishtick = System.nanoTime();
-
-			System.out.println("++ TIMING  +++++++++++++++++++++++++++++++++++++++++");
-			System.out.println("RESPONSE: " + (responsetick - starttick) / 1000000L + "ms");
-			System.out.println("FINISH:   " + (finishtick - starttick) / 1000000L + "ms");
-		}
+		System.out.println("++ TIMING  +++++++++++++++++++++++++++++++++++++++++");
+		System.out.println("LATENCY: " + client.getLatency() + "ms");
+		System.out.println("RESPONSE:   " + client.getResponseTime() + "ms");
+		System.out.println("++ CONTENT +++++++++++++++++++++++++++++++++++++++++");
+		System.out.println(client.getResponseContent());
 
 		return null;
 	}
 
 
-	public static void main(String[] args) throws ClientProtocolException, IOException {
-		HTTPPinger pinger = new HTTPPinger("http://google.com/");
+	public static void main(String[] args) throws ClientProtocolException, IOException, PingerServiceException {
+		HTTPPinger pinger = new HTTPPinger("http://localhost:8080/axirassa");
 		pinger.ping();
 	}
 }
