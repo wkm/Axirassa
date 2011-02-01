6c603bb70cbea61069e527ce847833bc9dbea1b1
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-01-31 22:39:08 -0700

    Implement PingerEntity.findPingerById and PingerEntity.findStatistics. Move query file into the designated resource directory.

--- src/main/java/axirassa/model/PingerEntity.java
+++ src/main/java/axirassa/model/PingerEntity.java
@@ -2,6 +2,7 @@
 package axirassa.model;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.Basic;
@@ -15,12 +16,40 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
+
 import axirassa.util.AutoSerializingObject;
 
 @Entity
 @Table(name = "Pingers")
 public class PingerEntity extends AutoSerializingObject implements Serializable {
 	private static final long serialVersionUID = -6709719920544228167L;
+
+
+	public static PingerEntity findPingerById(Session session, Long id) {
+		Query query = session.getNamedQuery("pinger_and_user_by_id");
+		query.setLong("id", id);
+
+		List<PingerEntity> pingers = query.list();
+		if (pingers.size() < 1)
+			return null;
+
+		return pingers.iterator().next();
+	}
+
+
+	public static List<HttpStatisticsEntity> findStatistics(Session session, PingerEntity pinger) {
+		Query query = session.getNamedQuery("pinger_statistics");
+		query.setEntity("pinger", pinger);
+
+		return query.list();
+	}
+
+
+	//
+	// INSTANCE
+	//
 
 	// ID
 	@Id
