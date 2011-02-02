0f78478c55c639e1f573344a7051ca96d7184f88
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-02-01 17:08:07 -0700

    Implement a Quartz powered controller runner. (the controller now runs every minute)

---
+++ src/main/java/axirassa/services/runners/ControllerServiceJob.java
@@ -0,0 +1,34 @@
+
+package axirassa.services.runners;
+
+import org.hibernate.Session;
+import org.hornetq.api.core.client.ClientSession;
+import org.quartz.Job;
+import org.quartz.JobDataMap;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import axirassa.services.ControllerService;
+import axirassa.services.Service;
+
+public class ControllerServiceJob implements Job {
+	public static String DATABASE_SESSION = "session.database";
+	public static String MESSAGING_SESSION = "session.messaging";
+
+
+	@Override
+	public void execute(JobExecutionContext context) throws JobExecutionException {
+		try {
+			JobDataMap datamap = context.getJobDetail().getJobDataMap();
+
+			ClientSession messagingSession = (ClientSession) datamap.get(MESSAGING_SESSION);
+			Session databaseSession = (Session) datamap.get(DATABASE_SESSION);
+
+			Service service = new ControllerService(messagingSession, databaseSession);
+
+			service.execute();
+		} catch (Exception e) {
+			throw new JobExecutionException(e);
+		}
+	}
+}
--- src/main/java/axirassa/services/runners/ControllerServiceRunner.java
+++ src/main/java/axirassa/services/runners/ControllerServiceRunner.java
@@ -3,23 +3,48 @@ package axirassa.services.runners;
 
 import org.hibernate.Session;
 import org.hornetq.api.core.client.ClientSession;
+import org.quartz.CronTrigger;
+import org.quartz.JobDataMap;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.Trigger;
+import org.quartz.impl.StdSchedulerFactory;
 
-import axirassa.services.ControllerService;
-import axirassa.services.Service;
+import axirassa.util.EmbeddedMessagingServer;
 import axirassa.util.HibernateTools;
 import axirassa.util.MessagingTools;
 
 public class ControllerServiceRunner {
+	public static String JOB_GROUP = "AxController";
+
+
+
+
+
+	enum ControllerJobs {
+		EVERY_MINUTE;
+	}
+
+
 	public static void main(String[] args) throws Exception {
-		ClientSession msgsession = MessagingTools.getEmbeddedSession();
-		Session dbsession = HibernateTools.getSession();
+		EmbeddedMessagingServer.start();
 
-		Service service = new ControllerService(msgsession, dbsession);
+		Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+		scheduler.start();
 
-		System.out.println("Executing controller");
-		service.execute();
-		System.out.println("Finished executing");
+		Session databaseSession = HibernateTools.getSession();
+		ClientSession messagingSession = MessagingTools.getEmbeddedSession();
 
+		JobDataMap datamap = new JobDataMap();
+		datamap.put(ControllerServiceJob.DATABASE_SESSION, databaseSession);
+		datamap.put(ControllerServiceJob.MESSAGING_SESSION, messagingSession);
+
+		JobDetail job = new JobDetail(ControllerJobs.EVERY_MINUTE.toString(), JOB_GROUP, ControllerServiceJob.class);
+		job.setJobDataMap(datamap);
+
+		Trigger trigger = new CronTrigger(ControllerJobs.EVERY_MINUTE.toString(), JOB_GROUP, "0 * * * * ?");
+
+		scheduler.scheduleJob(job, trigger);
 		return;
 	}
 }
