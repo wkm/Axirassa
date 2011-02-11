5a9e6134c1160a389431fb7d16b12213a651f911
wiktor <wiktor@zanoccio.com>
2011-02-10 19:10:54 -0700

    Working streaming with Chrome 9.5.

--- src/main/webapp/js/streaming.js
+++ src/main/webapp/js/streaming.js
@@ -1,6 +1,14 @@
 window.onload = function() {
-//	dojox.cometd.configure({
-//		url: "http://localhost:8080",
-//		logLevel: 'debug'
-//	});
+	var xmlhttp = new XMLHttpRequest;
+	var lastindex = 0;
+	
+	xmlhttp.open("GET", "http://localhost:8080/stream/", true);
+	xmlhttp.onreadystatechange = function() {
+		console.log("stage changed ---> ", xmlhttp.readyState);
+		var newindex = xmlhttp.responseText.length;
+		console.log("text: ", xmlhttp.responseText.substr(lastindex, newindex));
+		console.log("total bytes: ", newindex);
+		lastindex = newindex;
+	}
+	xmlhttp.send(null);
 };
