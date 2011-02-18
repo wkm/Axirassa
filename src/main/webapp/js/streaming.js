b3abcb8a08e949b7247c9d8ce2e8eb14d3d22ed0
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-02-18 15:49:21 -0700

    Try subscribing to two channels from one page. (works sucessfully)

--- src/main/webapp/js/streaming.js
+++ src/main/webapp/js/streaming.js
@@ -5,6 +5,10 @@ window.onload = function() {
 	dojox.cometd.handshake();
 	
 	var subscribe = dojox.cometd.subscribe("/ax/timeplease", function(msg){
-		dojo.byId("pollStatus").innerHTML = "Updated: " + msg.data;
+		dojo.byId("pollStatus1").innerHTML = "Updated: " + msg.data;
+	});
+	
+	var subscribe2 = dojox.cometd.subscribe("/ax/valueplease", function(msg){
+		dojo.byId("pollStatus2").innerHTML = "Updated: " + msg.data;
 	});
 };
