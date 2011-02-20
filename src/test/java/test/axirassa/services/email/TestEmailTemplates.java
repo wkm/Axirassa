c5facd6e24e6f97af4e99eccefe870258f72dc12
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-02-20 00:49:58 -0700

    Adjust e-mail template tests now that an e-mail template refers to three different string templates.

--- src/test/java/test/axirassa/services/email/TestEmailTemplates.java
+++ src/test/java/test/axirassa/services/email/TestEmailTemplates.java
@@ -6,11 +6,13 @@ import org.junit.Test;
 
 import axirassa.services.email.EmailTemplate;
 import axirassa.services.email.EmailTemplateFactory;
+import axirassa.services.email.EmailTemplateType;
 
 public class TestEmailTemplates {
 	@Test
 	public void testVerifyAccount() {
-		StringTemplate template = EmailTemplateFactory.getTemplateInstance(EmailTemplate.USER_VERIFY_ACCOUNT);
+		StringTemplate template = EmailTemplateFactory.getTemplateInstance(EmailTemplate.USER_VERIFY_ACCOUNT,
+		                                                                   EmailTemplateType.HTML);
 
 		template.setAttribute("email", "who@foo.com");
 		template.setAttribute("axlink", "http://axirassa.com/blahblahblah");
