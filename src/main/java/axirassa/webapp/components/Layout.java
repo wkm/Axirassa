05fe97f55741bd62773cc8b4784b57b046917082
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-02-20 00:48:08 -0700

    Link directly to fonts.css to prevent it from being re-requested. It doesn't seem to have helped with the refresh flicker.

--- src/main/java/axirassa/webapp/components/Layout.java
+++ src/main/java/axirassa/webapp/components/Layout.java
@@ -8,7 +8,7 @@ import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.tynamo.security.services.SecurityService;
 
-@Import(stylesheet = { "context:/css/fonts.css", "context:/css/form.css" })
+@Import(stylesheet = { "context:/css/form.css" })
 public class Layout {
 	@Inject
 	private SecurityService security;
