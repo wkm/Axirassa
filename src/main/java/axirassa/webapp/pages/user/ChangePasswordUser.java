8c3e6c19e2084f4864841db0fe7844afc62ff8cf
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-02-20 17:12:05 -0700

    Simplify the change password page for eventual removal

--- src/main/java/axirassa/webapp/pages/user/ChangePasswordUser.java
+++ src/main/java/axirassa/webapp/pages/user/ChangePasswordUser.java
@@ -1,32 +1,19 @@
 
 package axirassa.webapp.pages.user;
 
+import org.apache.shiro.authz.annotation.RequiresUser;
 import org.apache.tapestry5.annotations.Component;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.hibernate.annotations.CommitAfter;
-import org.apache.tapestry5.ioc.annotations.Inject;
-import org.hibernate.Session;
 
-import axirassa.model.PasswordResetTokenEntity;
-import axirassa.model.UserEntity;
 import axirassa.webapp.components.AxForm;
 import axirassa.webapp.components.AxPasswordField;
 
+@RequiresUser
 public class ChangePasswordUser {
-	@Inject
-	private Session session;
 
 	@Property
-	private String token;
-
-	@Property
-	private UserEntity user;
-
-	@Property
-	private boolean hasToken = false;
-
-	@Property
-	private boolean isTokenInvalid = false;
+	private final boolean hasToken = false;
 
 	@Property
 	private String currentPassword;
@@ -38,41 +25,23 @@ public class ChangePasswordUser {
 	private String confirmPassword;
 
 	@Component
+	private AxPasswordField currentPasswordField;
+
+	@Component
 	private AxPasswordField confirmPasswordField;
 
 	@Component
 	private AxForm form;
 
 
-	public Object onActivate(String token) {
-		if (token == null)
-			return true;
-
-		hasToken = true;
-		PasswordResetTokenEntity tokenEntity = PasswordResetTokenEntity.getByToken(session, token);
-
-		if (tokenEntity == null) {
-			isTokenInvalid = true;
-			return true;
-		}
-
-		isTokenInvalid = false;
-		user = tokenEntity.getUser();
-
-		return true;
-	}
-
-
 	public void onValidate() {
-		if (newPassword != null && confirmPassword != null && !newPassword.equals(confirmPassword))
-			form.recordError(confirmPasswordField, "Passwords do not match");
+
 	}
 
 
 	@CommitAfter
-	public String onSuccess() {
-		if (isTokenInvalid) {
-			user.createPassword(newPassword);
-		}
+	public Object onSuccess() {
+		return SettingsUser.class;
 	}
+
 }
