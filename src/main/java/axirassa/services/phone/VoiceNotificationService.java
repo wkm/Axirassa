10a81e0ae31ecaaad9d50a46344962e2bd18605c
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-02-27 00:01:15 -0700

    Bare-bones voice notification service.

--- src/main/java/axirassa/services/phone/VoiceNotificationService.java
+++ src/main/java/axirassa/services/phone/VoiceNotificationService.java
@@ -1,5 +1,21 @@
+
 package axirassa.services.phone;
 
-public class VoiceNotificationService {
+import org.hornetq.api.core.client.ClientSession;
 
+import axirassa.services.Service;
+
+public class VoiceNotificationService implements Service {
+	private final ClientSession messagingSession;
+
+
+	public VoiceNotificationService(ClientSession messagingSession) {
+		this.messagingSession = messagingSession;
+	}
+
+
+	@Override
+	public void execute() throws Exception {
+		System.err.println("EXECUTING VOICE NOTIFICATION SERVICE");
+	}
 }
