dc428c4954dd6d4b77d6d733c0cf02421ce5ca89
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-02-28 02:29:02 -0700

    Make VoiceNotifyService not extend SmsNotifyService since it confuses Tapestry, you can work around it using annotations, but it seems cleaner not to.

--- src/main/java/axirassa/webapp/services/VoiceNotifyService.java
+++ src/main/java/axirassa/webapp/services/VoiceNotifyService.java
@@ -1,6 +1,24 @@
 
 package axirassa.webapp.services;
 
-public interface VoiceNotifyService extends SmsNotifyService {
+import java.io.IOException;
+
+import org.hornetq.api.core.HornetQException;
+
+import axirassa.services.phone.PhoneTemplate;
+
+public interface VoiceNotifyService {
+	public abstract void startMessage(PhoneTemplate template);
+
+
+	public abstract void addAttribute(String key, String value);
+
+
+	public abstract void send() throws HornetQException, IOException;
+
+
+	public abstract void setPhoneNumber(String phoneNumber);
+
+
 	public abstract void setExtension(String extension);
 }
