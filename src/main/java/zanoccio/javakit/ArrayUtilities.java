86d86a6b7a84e1923396422c6a777db80140e45a
wiktor <wiktor@zanoccio.com>
2011-03-01 21:52:21 -0700

    Putting together an array utilities package

---
+++ src/main/java/zanoccio/javakit/ArrayUtilities.java
@@ -0,0 +1,9 @@
+package zanoccio.javakit;
+
+public class ArrayUtilities {
+	public String toString(byte[] array) {
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i= 0;i < array.length; i++) {
+	}
+}
