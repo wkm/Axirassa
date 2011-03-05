0536768c94039b0c738fcc77f594615901e5a40e
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-03-05 01:56:05 -0700

    Implement DatabaseMigrateService

---
+++ src/main/java/axirassa/services/DatabaseMigrateService.java
@@ -0,0 +1,33 @@
+
+package axirassa.services;
+
+import java.io.IOException;
+
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.exception.LiquibaseException;
+import liquibase.integration.commandline.CommandLineUtils;
+import liquibase.resource.ClassLoaderResourceAccessor;
+import liquibase.resource.ResourceAccessor;
+
+import org.hibernate.cfg.Configuration;
+
+public class DatabaseMigrateService {
+	public static final String MASTER_CHANGELOG = "axirassa/model/changelog/db.changelog-master.xml";
+
+
+	public static void main(String[] args) throws LiquibaseException, IOException {
+		Configuration config = new Configuration().configure();
+		ClassLoader classloader = DatabaseMigrateService.class.getClassLoader();
+
+		Database target = CommandLineUtils.createDatabaseObject(classloader, config.getProperty("connection.url"),
+		                                                        config.getProperty("connection.user"),
+		                                                        config.getProperty("connection.password"),
+		                                                        config.getProperty("connection.driver_class"), null,
+		                                                        null);
+
+		ResourceAccessor resourceAccessor = new ClassLoaderResourceAccessor(classloader);
+		Liquibase liquibase = new Liquibase(MASTER_CHANGELOG, resourceAccessor, target);
+		liquibase.update("");
+	}
+}
