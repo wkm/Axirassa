3a4e0a63042996247c941db948035cd9003a168e
wiktor <wiktor@zanoccio.com>
2011-03-21 14:27:42 -0600

    DAO for user phone numbers

---
+++ src/main/java/axirassa/dao/UserPhoneNumberDAO.java
@@ -0,0 +1,16 @@
+
+package axirassa.dao;
+
+import java.util.List;
+
+import axirassa.model.UserEntity;
+import axirassa.model.UserPhoneNumberEntity;
+
+public interface UserPhoneNumberDAO {
+
+	public abstract List<UserPhoneNumberEntity> getPhoneNumbersByUser(UserEntity user);
+
+
+	public abstract UserPhoneNumberEntity getByIdWithUser(long id);
+
+}
