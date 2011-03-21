80c0ffc3b60f1381e249f8d5967e750eb172a48d
wiktor <wiktor@zanoccio.com>
2011-03-21 14:27:26 -0600

    Module with DAO layer bindings

---
+++ src/main/java/axirassa/dao/DAOModule.java
@@ -0,0 +1,23 @@
+
+package axirassa.dao;
+
+import org.apache.tapestry5.hibernate.HibernateTransactionAdvisor;
+import org.apache.tapestry5.ioc.MethodAdviceReceiver;
+import org.apache.tapestry5.ioc.ServiceBinder;
+import org.apache.tapestry5.ioc.annotations.Match;
+
+public class DAOModule {
+	public static void bind(ServiceBinder binder) {
+		binder.bind(FeedbackDAO.class, FeedbackDAOImpl.class);
+		binder.bind(PasswordResetTokenDAO.class, PasswordResetTokenDAOImpl.class);
+		binder.bind(PingerDAO.class, PingerDAOImpl.class);
+		binder.bind(UserEmailAddressDAO.class, UserEmailAddressDAOImpl.class);
+	}
+
+
+	@Match("*DAO")
+	public static void adviseTransactions(HibernateTransactionAdvisor advisor, MethodAdviceReceiver receiver) {
+		advisor.addTransactionCommitAdvice(receiver);
+	}
+
+}
