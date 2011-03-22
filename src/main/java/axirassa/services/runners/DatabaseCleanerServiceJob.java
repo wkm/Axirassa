22b1ad17dea4820b4a14bac634c92e8ef2c72bee
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-03-22 14:21:32 -0600

    Use IoC in the database cleaner service job.

--- src/main/java/axirassa/services/runners/DatabaseCleanerServiceJob.java
+++ src/main/java/axirassa/services/runners/DatabaseCleanerServiceJob.java
@@ -1,22 +1,19 @@
+
 package axirassa.services.runners;
 
-
-import axirassa.services.DatabaseCleanerService;
-import axirassa.services.Service;
-import axirassa.util.HibernateTools;
-import org.hibernate.Session;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+import axirassa.ioc.IocStandalone;
+import axirassa.services.DatabaseCleanerService;
+import axirassa.services.Service;
 
 public class DatabaseCleanerServiceJob implements Job {
 	@Override
 	public void execute (JobExecutionContext context) throws JobExecutionException {
 		try {
-			Session database = HibernateTools.getLightweightSession();
-			Service service = new DatabaseCleanerService(database);
-
+			Service service = IocStandalone.autobuild(DatabaseCleanerService.class);
 			service.execute();
 		} catch (Exception e) {
 			throw new JobExecutionException(e);
