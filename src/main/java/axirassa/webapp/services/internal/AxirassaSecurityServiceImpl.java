b2ddb60600d3751c59c730f38fcb20479c6265d3
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-03-30 13:42:25 -0600

    Use normal Injection for AxirassaSecurityService instead of constructor-based injection

--- src/main/java/axirassa/webapp/services/internal/AxirassaSecurityServiceImpl.java
+++ src/main/java/axirassa/webapp/services/internal/AxirassaSecurityServiceImpl.java
@@ -1,72 +1,80 @@
-
 package axirassa.webapp.services.internal;
 
+import axirassa.dao.UserDAO;
+import axirassa.dao.UserEmailAddressDAO;
 import org.apache.shiro.subject.Subject;
 import org.hibernate.Session;
 import org.tynamo.security.services.SecurityService;
 
 import axirassa.model.EntityWithUser;
+import axirassa.model.UserEmailAddressEntity;
 import axirassa.model.UserEntity;
 import axirassa.webapp.services.AxirassaSecurityService;
 import axirassa.webapp.services.exceptions.AxirassaSecurityException;
+import org.apache.tapestry5.ioc.annotations.Inject;
 
 public class AxirassaSecurityServiceImpl implements AxirassaSecurityService {
 
-	private final SecurityService security;
+    @Inject
+    private SecurityService security;
+
+    @Inject
+    private UserDAO userDAO;
+
+    @Inject
+    private UserEmailAddressDAO userEmailAddressDAO;
+
+    @Override
+    public boolean isAuthenticated () {
+        return security.isAuthenticated();
+    }
 
 
-	public AxirassaSecurityServiceImpl(SecurityService security) {
-		this.security = security;
-	}
+    @Override
+    public boolean isGuest () {
+        return security.isGuest();
+    }
 
 
-	@Override
-	public boolean isAuthenticated() {
-		return security.isAuthenticated();
-	}
+    @Override
+    public boolean isUser () {
+        return security.isUser();
+    }
 
 
-	@Override
-	public boolean isGuest() {
-		return security.isGuest();
-	}
+    @Override
+    public String getEmail () {
+        return (String) security.getSubject().getPrincipal();
+    }
 
 
-	@Override
-	public boolean isUser() {
-		return security.isUser();
-	}
+    @Override
+    public Subject getSubject () {
+        return security.getSubject();
+    }
 
 
-	@Override
-	public String getEmail() {
-		return (String) security.getSubject().getPrincipal();
-	}
+    @Override
+    public void verifyOwnership (EntityWithUser entity) throws AxirassaSecurityException {
+        if (entity.getUser() == null)
+            throw new AxirassaSecurityException();
+
+        UserEntity user = entity.getUser();
+        UserEmailAddressEntity emailEntity = userEmailAddressDAO.getPrimaryEmail(user);
+
+        if (!getEmail().equals(emailEntity.getEmail()))
+            throw new AxirassaSecurityException();
+    }
 
 
-	@Override
-	public Subject getSubject() {
-		return security.getSubject();
-	}
+    @Override
+    public UserEntity getUserEntity (Session database) {
+        String email = getEmail();
+        if (email == null)
+            return null;
 
+        return userDAO.getUserByEmail(getEmail());
+    }
 
-	@Override
-	public void verifyOwnership(EntityWithUser entity) throws AxirassaSecurityException {
-		if (entity.getUser() == null)
-			throw new AxirassaSecurityException();
-
-		if (!getEmail().equals(entity.getUser().getEmail()))
-			throw new AxirassaSecurityException();
-	}
-
-
-	@Override
-	public UserEntity getUserEntity(Session database) {
-		String email = getEmail();
-		if (email == null)
-			return null;
-
-		return UserEntity.getUserByEmail(database, getEmail());
-	}
 
 }
