c68dc8dbf0bb1179a6d828c44c640ecf61aac408
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-03-30 13:43:14 -0600

    Simple test for the create user flow

--- src/test/java/test/axirassa/flows/TestCreateUserFlow.java
+++ src/test/java/test/axirassa/flows/TestCreateUserFlow.java
@@ -1,10 +1,37 @@
-
 package test.axirassa.flows;
 
 import axirassa.ioc.IocTestRunner;
+import axirassa.model.UserEntity;
+import axirassa.model.flows.CreateUserFlow;
+import java.util.List;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertFalse;
 
 @RunWith(IocTestRunner.class)
 public class TestCreateUserFlow {
+
+    @Inject
+    private CreateUserFlow createUserFlow;
+
+    @Inject
+    private Session database;
+
+    @Test
+    public void createUser () {
+        createUserFlow.setEmail("who@foo.com");
+        createUserFlow.setPassword("password");
+        createUserFlow.execute();
+
+        Query query = database.createQuery("from UserEntity");
+        List<UserEntity> users = query.list();
+
+        assertFalse(users.isEmpty());
+    }
+
 
 }
