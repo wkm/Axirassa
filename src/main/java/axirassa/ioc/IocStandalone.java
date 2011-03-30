b455f60fe914e8018e9e326f5db9600cc622b870
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-03-30 13:41:38 -0600

    Include FlowsModule when building the registry

--- src/main/java/axirassa/ioc/IocStandalone.java
+++ src/main/java/axirassa/ioc/IocStandalone.java
@@ -1,28 +1,30 @@
-
 package axirassa.ioc;
 
 import org.apache.tapestry5.ioc.Registry;
 import org.apache.tapestry5.ioc.RegistryBuilder;
 
 public class IocStandalone {
-	public static Registry registry = null;
+
+    public static Registry registry = null;
+
+    public static void init () {
+        if (registry == null) {
+            RegistryBuilder builder = new RegistryBuilder();
+            builder.add(DAOModule.class);
+            builder.add(FlowsModule.class);
+
+            registry = builder.build();
+            registry.performRegistryStartup();
+        }
+    }
 
 
-	public static void init () {
-		if (registry == null) {
-			RegistryBuilder builder = new RegistryBuilder();
-			builder.add(DAOModule.class);
+    public static <T> T autobuild (Class<T> classObject) {
+        if (registry == null)
+            init();
 
-			registry = builder.build();
-			registry.performRegistryStartup();
-		}
-	}
+        return registry.autobuild(classObject);
+    }
 
 
-	public static <T> T autobuild (Class<T> classObject) {
-		if (registry == null)
-			init();
-
-		return registry.autobuild(classObject);
-	}
 }
