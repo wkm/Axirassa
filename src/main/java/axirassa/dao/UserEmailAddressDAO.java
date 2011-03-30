969a490d031034d77528b6615cf594c5b3bccabf
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-03-30 13:41:23 -0600

    make #getPrimaryEmail public

--- src/main/java/axirassa/dao/UserEmailAddressDAO.java
+++ src/main/java/axirassa/dao/UserEmailAddressDAO.java
@@ -1,4 +1,3 @@
-
 package axirassa.dao;
 
 import java.util.List;
@@ -8,37 +7,44 @@ import axirassa.model.UserEntity;
 
 public interface UserEmailAddressDAO {
 
-	/**
-	 * @return the {@link UserEmailAddressEntity} and an eagerly-fetched
-	 *         <tt>user</tt> property, or null if no e-mail with the given id
-	 *         exists.
-	 */
-	public UserEmailAddressEntity getByIdWithUser(long id);
+    /**
+     * @return the {@link UserEmailAddressEntity} and an eagerly-fetched
+     *         <tt>user</tt> property, or null if no e-mail with the given id
+     *         exists.
+     */
+    public UserEmailAddressEntity getByIdWithUser (long id);
 
 
-	/**
-	 * @return all email addresses associated with a user
-	 */
-	public List<UserEmailAddressEntity> getEmailsByUser(UserEntity user);
+    /**
+     * @return all email addresses associated with a user
+     */
+    public List<UserEmailAddressEntity> getEmailsByUser (UserEntity user);
 
 
-	/**
-	 * @return the email address associated with the given token, or null if no
-	 *         such e-mail exists.
-	 */
-	public UserEmailAddressEntity getByToken(String token);
+    /**
+     * @return the email address associated with the given token, or null if no
+     *         such e-mail exists.
+     */
+    public UserEmailAddressEntity getByToken (String token);
 
 
-	/**
-	 * @return true if the e-mail is taken.
-	 */
-	public boolean isEmailRegistered(String email);
+    /**
+     * @return true if the e-mail is taken.
+     */
+    public boolean isEmailRegistered (String email);
 
 
-	/**
-	 * @return the user associated with this e-mail.
-	 */
-	public UserEntity getUserByEmail(String email);
+    /**
+     * @return the user associated with this e-mail.
+     */
+    public UserEntity getUserByEmail (String email);
 
-    UserEmailAddressEntity getPrimaryEmail (UserEntity user);
+
+    /**
+     * @return the primary e-mail address entity associated with this user 
+     *         (their authentication e-mail)
+     */
+    public UserEmailAddressEntity getPrimaryEmail (UserEntity user);
+
+
 }
