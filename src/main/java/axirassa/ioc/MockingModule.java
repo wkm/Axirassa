5747e266f388f8714a9d20aa5a6bace9e8565a79
wiktor <wiktor@zanoccio.com>
2011-04-02 16:24:01 -0600

    Mock PageRenderLinkSource

--- src/main/java/axirassa/ioc/MockingModule.java
+++ src/main/java/axirassa/ioc/MockingModule.java
@@ -6,6 +6,7 @@ import axirassa.webapp.services.MessagingSession;
 import axirassa.webapp.services.MessagingSessionManager;
 import axirassa.webapp.services.SmsNotifyService;
 import axirassa.webapp.services.VoiceNotifyService;
+import org.apache.tapestry5.services.PageRenderLinkSource;
 import org.hibernate.Session;
 import static org.mockito.Mockito.mock;
 
@@ -16,13 +17,17 @@ import static org.mockito.Mockito.mock;
  */
 public class MockingModule {
 
-    public static void contributeServiceOverride(MappedConfiguration<Class, Object> configuration) {
+    public static void contributeServiceOverride (MappedConfiguration<Class, Object> configuration) {
         configuration.add(Session.class, mock(Session.class));
         configuration.add(MessagingSessionManager.class, mock(MessagingSessionManager.class));
         configuration.add(MessagingSession.class, mock(MessagingSession.class));
         configuration.add(EmailNotifyService.class, mock(EmailNotifyService.class));
         configuration.add(SmsNotifyService.class, mock(SmsNotifyService.class));
         configuration.add(VoiceNotifyService.class, mock(VoiceNotifyService.class));
+
+        PageRenderLinkSource mockPrls = mock(PageRenderLinkSource.class);
+        configuration.add(PageRenderLinkSource.class, mockPrls);
     }
+
 
 }
