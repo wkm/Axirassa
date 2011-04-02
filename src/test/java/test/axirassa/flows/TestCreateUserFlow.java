767d2a2dc18794260a0bf2518064460dd6b78c7f
wiktor <wiktor@zanoccio.com>
2011-04-02 16:11:09 -0600

    Trying to unit-test CreateUserFlow

--- src/test/java/test/axirassa/flows/TestCreateUserFlow.java
+++ src/test/java/test/axirassa/flows/TestCreateUserFlow.java
@@ -1,8 +1,10 @@
 package test.axirassa.flows;
 
-import axirassa.ioc.IocIntegrationTestRunner;
+import axirassa.webapp.services.EmailNotifyService;
+import axirassa.ioc.IocUnitTestRunner;
 import axirassa.model.UserEntity;
 import axirassa.model.flows.CreateUserFlow;
+import axirassa.overlord.exceptions.ExceptionInMonitorError;
 import java.util.List;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.hibernate.Query;
@@ -11,8 +13,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertFalse;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
 
-@RunWith(IocIntegrationTestRunner.class)
+@RunWith(IocUnitTestRunner.class)
 public class TestCreateUserFlow {
 
     @Inject
@@ -21,8 +26,14 @@ public class TestCreateUserFlow {
     @Inject
     private Session database;
 
+    @Inject
+    private EmailNotifyService emailer;
+
     @Test
     public void createUser () {
+        System.out.println("SESSION: "+database);
+        when(database.save(any())).thenThrow(new ExceptionInInitializerError("Big PROBLEM"));
+
         createUserFlow.setEmail("who@foo.com");
         createUserFlow.setPassword("password");
         createUserFlow.execute();
