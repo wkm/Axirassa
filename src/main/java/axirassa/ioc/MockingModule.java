b64346816eeeadc73838a3876bada044b4fcec7e
wiktor <wiktor@zanoccio.com>
2011-04-02 16:07:48 -0600

    Use service overrides to correctly implement mock services with mockito

--- src/main/java/axirassa/ioc/MockingModule.java
+++ src/main/java/axirassa/ioc/MockingModule.java
@@ -1,41 +1,34 @@
 package axirassa.ioc;
 
+import org.apache.tapestry5.ioc.MappedConfiguration;
 import axirassa.webapp.services.EmailNotifyService;
 import axirassa.webapp.services.MessagingSession;
 import axirassa.webapp.services.MessagingSessionManager;
 import axirassa.webapp.services.SmsNotifyService;
 import axirassa.webapp.services.VoiceNotifyService;
+import org.apache.tapestry5.ioc.ScopeConstants;
+import org.apache.tapestry5.ioc.annotations.Scope;
 import org.hibernate.Session;
 import static org.mockito.Mockito.mock;
+
 /**
  * Provides mock implementations of common services
  *
  * @author wiktor
  */
 public class MockingModule {
-    // DATABASE MOCKS
-    public static Session decorateSession() {
-        return mock(Session.class);
+
+    public MockingModule () {
+        System.err.println("CREATING MOCKING MODULE");
     }
 
-    // MESSAGING MOCKS
-    public static MessagingSessionManager decorateMessagingSessionManager() {
-        return mock(MessagingSessionManager.class);
+    public static void contributeServiceOverride(MappedConfiguration<Class, Object> configuration) {
+        configuration.add(Session.class, mock(Session.class));
+        configuration.add(MessagingSessionManager.class, mock(MessagingSessionManager.class));
+        configuration.add(MessagingSession.class, mock(MessagingSession.class));
+        configuration.add(EmailNotifyService.class, mock(EmailNotifyService.class));
+        configuration.add(SmsNotifyService.class, mock(SmsNotifyService.class));
+        configuration.add(VoiceNotifyService.class, mock(VoiceNotifyService.class));
     }
 
-    public static MessagingSession decorateMessagingSession() {
-        return mock(MessagingSession.class);
-    }
-
-    public static EmailNotifyService decorateEmailNotifyService() {
-        return mock(EmailNotifyService.class);
-    }
-
-    public static SmsNotifyService decorateSmsNotifyService() {
-        return mock(SmsNotifyService.class);
-    }
-
-    public static VoiceNotifyService decorateVoiceNotifyService() {
-        return mock(VoiceNotifyService.class);
-    }
 }
