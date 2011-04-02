2e80cd5065f6faf944f48f9dd152454bbfc89027
wiktor <wiktor@zanoccio.com>
2011-04-01 21:27:25 -0600

    Slight formatting tweak

--- src/main/java/axirassa/dao/UserDAO.java
+++ src/main/java/axirassa/dao/UserDAO.java
@@ -1,11 +1,10 @@
-
 package axirassa.dao;
 
 import axirassa.model.UserEntity;
 
 public interface UserDAO {
-	boolean isEmailRegistered (String email);
 
+    boolean isEmailRegistered(String email);
 
-	UserEntity getUserByEmail (String email);
+    UserEntity getUserByEmail(String email);
 }
