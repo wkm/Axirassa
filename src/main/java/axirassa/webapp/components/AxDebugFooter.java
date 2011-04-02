4d61c0b1baf2f987f340312c97d9831b584d1a88
wiktor <wiktor@zanoccio.com>
2011-04-01 21:15:56 -0600

    Show the session max inactive interval if a session is defined

--- src/main/java/axirassa/webapp/components/AxDebugFooter.java
+++ src/main/java/axirassa/webapp/components/AxDebugFooter.java
@@ -1,25 +1,33 @@
-
 package axirassa.webapp.components;
 
 import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.Request;
+import org.apache.tapestry5.services.Session;
 
 public class AxDebugFooter {
-	@Inject
-	@Property
-	private Request request;
 
-	@Property
-	private List<String> headers;
+    @Inject
+    @Property
+    private Request request;
 
-	@Property
-	private String header;
+    @Property
+    private List<String> headers;
+
+    @Property
+    private String header;
+
+    @Property
+    private Session session;
+
+    public void setupRender () {
+        session = request.getSession(false);
+        headers = request.getHeaderNames();
+    }
 
 
-	public void setupRender() {
-		headers = request.getHeaderNames();
-	}
 }
