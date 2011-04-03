f833298a26b938a092c1b894882b52d46b044b2e
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-04-03 13:51:07 -0600

    Actually show logging output

--- src/main/java/axirassa/webapp/ajax/httpstream/HttpStreamingTransport.java
+++ src/main/java/axirassa/webapp/ajax/httpstream/HttpStreamingTransport.java
@@ -17,60 +17,59 @@ public class HttpStreamingTransport extends HttpTransport {
 	public final static String PREFIX = "http-streaming";
 
 
-	public HttpStreamingTransport(BayeuxServerImpl bayeux, String name) {
+	public HttpStreamingTransport (BayeuxServerImpl bayeux, String name) {
 		super(bayeux, name);
-
 		setOptionPrefix(PREFIX);
 
 		info("constructed");
 	}
 
 
-	protected Logger getLogger() {
+	protected Logger getLogger () {
 		return getBayeux().getLogger();
 	}
 
 
-	protected void info(Object... args) {
-		if (getLogger().isDebugEnabled()) {
-			StringBuffer sb = new StringBuffer("#### HTTP STREAMING: ");
-			for (Object arg : args)
-				if (arg == null)
-					sb.append("null ");
-				else
-					sb.append(arg.toString()).append(' ');
+	protected void info (Object... args) {
+		StringBuffer sb = new StringBuffer("#### HTTP STREAMING: ");
+		for (Object arg : args)
+			if (arg == null)
+				sb.append("null ");
+			else
+				sb.append(arg.toString()).append(' ');
 
-			getLogger().debug(sb.toString());
-		}
+		System.err.println(sb.toString());
 	}
 
 
-	public ServerMessage.Mutable[] parseRequestMessages(HttpServletRequest request) throws IOException, ParseException {
+	public ServerMessage.Mutable[] parseRequestMessages (HttpServletRequest request) throws IOException, ParseException {
 		return parseMessages(request);
 	}
 
 
 	@Override
-	public long getMaxInterval() {
+	public long getMaxInterval () {
 		return 60000;
 	}
 
 
 	@Override
-	protected void init() {
+	protected void init () {
 		super.init();
 		info("initializing");
 	}
 
 
 	@Override
-	public boolean accept(HttpServletRequest request) {
+	public boolean accept (HttpServletRequest request) {
+		info("ACCEPTING REQUEST: ", request);
 		return "POST".equals(request.getMethod());
 	}
 
 
 	@Override
-	public void handle(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+	public void handle (HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		info("HANLDING REQUEST AND RESPONSE");
 		HttpStreamingTransportHandler handler = new HttpStreamingTransportHandler(this, request, response);
 		handler.handle();
 	}
