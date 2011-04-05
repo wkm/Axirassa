8c578ac2031225f550186c5a884f59c9a1723f97
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-04-04 22:49:14 -0600

    Temporarily disable cometd streaming and just work on the initial rendering of chart data.

--- src/main/webapp/js/axwidget.js
+++ src/main/webapp/js/axwidget.js
@@ -89,6 +89,37 @@ function inspect(object, depth) {
 }
 
 function AxPlot(id, pingerId) {
+	// pull the original data
+	dojo.xhrGet({
+			url: '/monitorconsole.axmonitorwidget/'+pingerId,
+			handleAs: 'json',
+			load: function(data) {
+				var chart = new dojox.charting.Chart2D(id + "_plot");
+				
+				var i;
+				var responseSize = [];
+				for(i = 0; i < data.length; i++) {
+					responseSize[i] = data[i][1];
+				}
+				var smoother = weightedMovingAverage(responseSize, 5);
+				responseSize = responseSize.slice(5);
+				
+				chart.setTheme(colorTheme);
+				chart.addPlot('smooth', {
+					type: 'Lines',
+					lines: true,
+			        tension: 3
+				});
+			    chart.addPlot('raw', {type: 'Lines', lines: true});
+				
+				chart.addSeries('Response Time', smoother, {plot:'smooth'});
+				chart.addSeries('Response Size', responseSize, {plot:'raw'});
+				
+				chart.render();
+				dojo.destroy(id+"_spin");
+			}
+	});
+	
     var data = randomData(200);
     var smoothed = weightedMovingAverage(data, 10);
     data = data.slice(10);
@@ -98,19 +129,7 @@ function AxPlot(id, pingerId) {
 		responseSize: data
 	};
 	
-	var chart = new dojox.charting.Chart2D(id + "_plot");
-	chart.setTheme(colorTheme);
-	chart.addPlot('smooth', {
-		type: 'Lines',
-		lines: true,
-        tension: 2
-	});
-    chart.addPlot('raw', {type: 'Lines', lines: true});
 	
-	chart.addSeries('Response Time', json['responseTime'], {plot:'smooth'});
-	chart.addSeries('Response Size', json['responseSize'], {plot:'raw'});
-	
-	chart.render();
 	
 	
 	if(!connected) {
@@ -118,17 +137,17 @@ function AxPlot(id, pingerId) {
 	} else {
 		console.log("already connected");
 	}
-	
-	console.log("SUBSCRIBING TO: "+ pingerId);
-	dojox.cometd.subscribe("/ax/pinger/"+pingerId, function(msg){
-		console.log("Received data point: for ["+pingerId+"] " + msg.data);
-		dojo.byId(id + "_time").innerHTML = msg.data;
-		dojo.byId(id + "_sz").innerHTML = msg.data['responseSize'] + "<span class='u'>b</span>";
-		dojo.byId(id + "_t").innerHtml = msg.data['responseTime'] + "<span class='u'>ms</span>";
-	});
-	dojox.cometd.subscribe("/ax/timeplease", function(msg) {
-		dojo.byId(id + "_timestamp").innerHTML = msg.data;
-	});
+//	
+//	console.log("SUBSCRIBING TO: "+ pingerId);
+//	dojox.cometd.subscribe("/ax/pinger/"+pingerId, function(msg){
+//		console.log("Received data point: for ["+pingerId+"] " + msg.data);
+//		dojo.byId(id + "_time").innerHTML = msg.data;
+//		dojo.byId(id + "_sz").innerHTML = msg.data['responseSize'] + "<span class='u'>b</span>";
+//		dojo.byId(id + "_t").innerHtml = msg.data['responseTime'] + "<span class='u'>ms</span>";
+//	});
+//	dojox.cometd.subscribe("/ax/timeplease", function(msg) {
+//		dojo.byId(id + "_timestamp").innerHTML = msg.data;
+//	});
 }
 function connectStreaming() {
 	if(!connected) {
