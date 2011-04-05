3f8db66444ed4553631e6e850238c95611dfa366
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-04-04 22:47:32 -0600

    Provide an #onAction method which returns the intial data-packet associated with a monitor (last six hours)

--- src/main/java/axirassa/webapp/components/AxMonitorWidget.java
+++ src/main/java/axirassa/webapp/components/AxMonitorWidget.java
@@ -1,19 +1,26 @@
+
 package axirassa.webapp.components;
 
+import java.util.List;
 
-import axirassa.dao.PingerDAO;
-import axirassa.model.PingerEntity;
 import org.apache.tapestry5.annotations.Import;
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.json.JSONArray;
 import org.apache.tapestry5.services.javascript.JavaScriptSupport;
+import org.apache.tapestry5.util.TextStreamResponse;
 import org.hibernate.Session;
 
+import axirassa.dao.PingerDAO;
+import axirassa.model.HttpStatisticsEntity;
+import axirassa.model.PingerEntity;
+import axirassa.webapp.services.AxirassaSecurityService;
+import axirassa.webapp.services.exceptions.AxirassaSecurityException;
 
 @Import(
-		stylesheet = { "context:/css/axwidget.css" },
-		library = { "context:/js/dojo/dojo.js", "context:/js/axwidget.js" })
+        stylesheet = { "context:/css/axwidget.css" },
+        library = { "context:/js/dojo/dojo.js", "context:/js/axwidget.js" })
 public class AxMonitorWidget {
 
 	@Inject
@@ -25,6 +32,8 @@ public class AxMonitorWidget {
 	@Inject
 	private PingerDAO pingerDAO;
 
+	@Inject
+	private AxirassaSecurityService security;
 
 	@Parameter(required = true, defaultPrefix = "literal")
 	@Property
@@ -49,8 +58,29 @@ public class AxMonitorWidget {
 			return;
 		}
 
-		// temporary hack to fix 
+		// temporary hack to fix
 		pingerName = pinger.getUrl().replace("http://", "");
+
+	}
+
+
+	public Object onAction (long pingerId) throws AxirassaSecurityException {
+		System.err.println("ATTEMPTING TO HANDLE ACTION");
+
+		pinger = pingerDAO.findPingerById(pingerId);
+		security.verifyOwnership(pinger);
+
+		List<HttpStatisticsEntity> dataPoints = pingerDAO.getDataPoints(pinger, PingerDAO.SIX_HOURS);
+
+		JSONArray array = new JSONArray();
+
+		for (HttpStatisticsEntity datum : dataPoints) {
+			JSONArray line = new JSONArray(datum.getTimestampInMillis(), datum.getResponseTime(),
+			        datum.getResponseSize());
+			array.put(line);
+		}
+
+		return new TextStreamResponse("text/plain", array.toCompactString());
 	}
 
 
