e3592f1acd6d11addbb905b0fc6c3299389e13b6
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-04-13 15:47:29 -0600

    Use AxFormField; remove label hard-coding into AxButtonBar.
    --------------------------------------------------
    ACTIVE - BugzID:157: Implement on-click styling for AxButtonBar
    https://zanoccio.fogbugz.com/default.asp?157

--- src/main/java/axirassa/webapp/components/AxButtonBar.java
+++ src/main/java/axirassa/webapp/components/AxButtonBar.java
@@ -2,14 +2,9 @@
 package axirassa.webapp.components;
 
 import org.apache.tapestry5.annotations.Import;
-import org.apache.tapestry5.annotations.Parameter;
-import org.apache.tapestry5.annotations.Property;
 
 @Import(
         library = { "context:js/dojo/dojo.js", "context:js/axbuttonbar.js" },
         stylesheet = "context:css/axbuttonbar.css")
 public class AxButtonBar {
-	@Property
-	@Parameter(defaultPrefix = "literal")
-	private String name;
 }
