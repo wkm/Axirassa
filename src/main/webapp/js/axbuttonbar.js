fe64e69a1a9023f58f16522f9818f9483fd3dcad
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-04-13 18:03:15 -0600

    Correctly style button bars with default selections.
    --------------------------------------------------
    ACTIVE - BugzID:160: Better stylize add e-mail, add phone number buttons
    https://zanoccio.fogbugz.com/default.asp?160

--- src/main/webapp/js/axbuttonbar.js
+++ src/main/webapp/js/axbuttonbar.js
@@ -1,14 +1,14 @@
+
+var $buttonbar = {};
+
 dojo.ready(function() {
 	var buttonbars = dojo.query(".bb");
-	var i = 0;
 	var inner;
-	for(; i < buttonbars.length; i++) {
-		console.log('button bar: ' + buttonbars[i]);
+	dojo.query('.bb').forEach(function(buttonbar, i, nodelist) {
 		dojo.query('label', buttonbars[i]).forEach(function(node, index, nodelist){
-			console.log('node: '+node + '   ' + index + ' of '+nodelist.length);
 			dojo.addClass(node, 'bbb');
 			inner = node.innerHTML;
-			node.innerHTML = null;
+			node.innerHTML = '';
 			
 			dojo.create('div', {
 				innerHTML: inner,
@@ -22,9 +22,27 @@ dojo.ready(function() {
 			if(index == nodelist.length - 1) {
 				dojo.addClass(node, 'bbbr');
 			}
+			
+			$buttonbar[buttonbars[i]] = {};
+
+			var input = dojo.byId(dojo.attr(node, 'for'));
+			if(input != null && dojo.attr(input, 'checked') == true) {
+				buttonbar["active"] = node;
+				dojo.addClass(node, 'active');
+			}
+			
+			dojo.connect(node, "onclick", function(evt){
+				var activenode = buttonbar["active"];
+				if(activenode != null) {
+					dojo.removeClass(activenode, 'active');
+				}
+				
+				buttonbar["active"] = node;
+				dojo.addClass(node, 'active');
+			});
 		});
 		
 		dojo.create("div", {className: "reset"}, buttonbars[i]);
-	}
+	});
 
 });
