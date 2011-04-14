f4b5cc04905f7809df17b56bb6fa27857729f5e7
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-04-14 14:25:54 -0600

    Very basic formatting for triggers (just give the short class name) -------------------------------------------------- ACTIVE - BugzID:162: Could not connect exception https://zanoccio.fogbugz.com/default.asp?162

--- src/main/java/axirassa/trigger/AbstractTrigger.java
+++ src/main/java/axirassa/trigger/AbstractTrigger.java
@@ -9,4 +9,9 @@ public class AbstractTrigger implements Trigger {
 	@Getter
 	@Setter
 	private PingerEntity pinger;
+	
+	@Override
+    public String toString() {
+		return getClass().getSimpleName();
+	}
 }
