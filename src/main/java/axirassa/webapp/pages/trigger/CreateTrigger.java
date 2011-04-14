d51e842efe58f4e8f159282ce28553e244b3ed9f
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-04-13 18:02:48 -0600

    Require a user to create a trigger.
    --------------------------------------------------
    ACTIVE - BugzID:160: Better stylize add e-mail, add phone number buttons
    https://zanoccio.fogbugz.com/default.asp?160

--- src/main/java/axirassa/webapp/pages/trigger/CreateTrigger.java
+++ src/main/java/axirassa/webapp/pages/trigger/CreateTrigger.java
@@ -1,8 +1,10 @@
 
 package axirassa.webapp.pages.trigger;
 
+import org.apache.shiro.authz.annotation.RequiresUser;
 import org.apache.tapestry5.annotations.Property;
 
+@RequiresUser
 public class CreateTrigger {
 	@Property
 	private String latency;
