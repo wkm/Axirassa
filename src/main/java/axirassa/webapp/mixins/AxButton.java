4d3972e53a5a023403efa1e0a1c211152230e73f
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-04-15 13:07:54 -0600

    This is why tapestry is amazing. With one line fix AxButton to injecting within the <a> instead of wrapping the outside.
    --------------------------------------------------
    ACTIVE - BugzID:174: AxButton - modify AxButton to inject within the <a>
    https://zanoccio.fogbugz.com/default.asp?174

--- src/main/java/axirassa/webapp/mixins/AxButton.java
+++ src/main/java/axirassa/webapp/mixins/AxButton.java
@@ -3,8 +3,10 @@ package axirassa.webapp.mixins;
 
 import org.apache.tapestry5.MarkupWriter;
 import org.apache.tapestry5.annotations.Import;
+import org.apache.tapestry5.annotations.MixinAfter;
 
 @Import(stylesheet = { "context:/css/axbutton.css" })
+@MixinAfter
 public class AxButton {
 
 	void beginRender (MarkupWriter writer) {
