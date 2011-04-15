5c52f109c197d0e6897709574617fac42262645d
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-04-15 13:25:59 -0600

    Support for tight parameter to AxButton
    --------------------------------------------------
    RESOLVED (IMPLEMENTED) - BugzID:175: AxButton - add a mixin parameter for a tighter button (not fixed width)
    https://zanoccio.fogbugz.com/default.asp?175

--- src/main/java/axirassa/webapp/mixins/AxButton.java
+++ src/main/java/axirassa/webapp/mixins/AxButton.java
@@ -1,16 +1,31 @@
 
 package axirassa.webapp.mixins;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import org.apache.tapestry5.MarkupWriter;
 import org.apache.tapestry5.annotations.Import;
 import org.apache.tapestry5.annotations.MixinAfter;
+import org.apache.tapestry5.annotations.Parameter;
 
 @Import(stylesheet = { "context:/css/axbutton.css" })
 @MixinAfter
 public class AxButton {
 
+	@Parameter
+	@Getter
+	@Setter
+	private boolean tight;
+
+
 	void beginRender (MarkupWriter writer) {
-		writer.element("div", "class", "button");
+		StringBuilder sb = new StringBuilder("button");
+		
+		if(tight)
+			sb.append(" tight");
+		
+		writer.element("div", "class", sb.toString());
 		writer.element("div", "class", "innerbutton");
 	}
 
