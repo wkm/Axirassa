6e299df13cbef9328d607972b02ef5e6ffeebbf2
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-04-15 00:15:49 -0600

    Remove references of monitor type entity in tests.

--- src/test/java/test/axirassa/domain/TestPingerEntity.java
+++ src/test/java/test/axirassa/domain/TestPingerEntity.java
@@ -4,7 +4,6 @@ package test.axirassa.domain;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
-import java.util.Collections;
 
 import org.apache.tapestry5.hibernate.annotations.CommitAfter;
 import org.apache.tapestry5.ioc.annotations.Inject;
@@ -14,7 +13,6 @@ import org.junit.runner.RunWith;
 
 import axirassa.ioc.IocIntegrationTestRunner;
 import axirassa.model.MonitorType;
-import axirassa.model.MonitorTypeEntity;
 import axirassa.model.PingerEntity;
 import axirassa.model.PingerFrequency;
 import axirassa.model.UserEntity;
@@ -32,25 +30,21 @@ public class TestPingerEntity {
 
 	@Test
 	@CommitAfter
-	public void testPingerSize() throws IOException {
+	public void testPingerSize () throws IOException {
 		UserEntity user = new UserEntity();
 		user.createPassword("password");
 		database.save(user);
 
-		MonitorTypeEntity type = new MonitorTypeEntity();
-		type.setType(MonitorType.HTTP);
-
 		PingerEntity pinger = new PingerEntity();
 		pinger.setFrequency(PingerFrequency.MINUTE);
-		pinger.setMonitorType(Collections.singleton(type));
 		pinger.setUrl("www.google.com");
 		pinger.setUser(user);
+		pinger.setMonitorType(MonitorType.HTTP);
 
 		database.save(user);
 		// create many of these pingers
 		for (int i = 0; i < 100; i++)
 			database.save(pinger);
-		database.save(type);
 
 		assertTrue(pinger.toBytes().length < 1500);
 	}
