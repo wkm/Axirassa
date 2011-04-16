3821d5087b8242d2a8f0156055752d51eb64aac2
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-04-16 00:17:53 -0600

    Fix import for the proper HttpClient
    --------------------------------------------------
    ACTIVE - BugzID:180: Fix Gzip compression of data through nginx proxy
    https://zanoccio.fogbugz.com/default.asp?180

--- src/main/java/axirassa/trigger/NoResponseTrigger.java
+++ src/main/java/axirassa/trigger/NoResponseTrigger.java
@@ -3,12 +3,12 @@ package axirassa.trigger;
 
 import lombok.Getter;
 
-import org.apache.commons.httpclient.NoHttpResponseException;
+import org.apache.http.NoHttpResponseException;
 
 public class NoResponseTrigger extends AbstractTrigger {
 	@Getter
 	private NoHttpResponseException cause;
-
+ 
 
 	public NoResponseTrigger (NoHttpResponseException cause) {
 		this.cause = cause;
