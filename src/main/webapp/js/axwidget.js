c70d55c41eb68b85dc0bc04f34fffab0508262ee
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-04-15 23:39:31 -0600

    Major cleanups to the monitor widget javascript

--- src/main/webapp/js/axwidget.js
+++ src/main/webapp/js/axwidget.js
@@ -2,7 +2,8 @@ var connected = false;
 
 dojo.require("dojox.charting.widget.Chart2D");
 dojo.require("dojox.charting.Theme");
-dojo.require("dojox.charting.themes.Tom");
+dojo.require("dojox.charting.action2d.Tooltip");
+dojo.require("dojox.charting.action2d.Magnify");
 
 var colorTheme = new dojox.charting.Theme({
 	chart: {
@@ -34,8 +35,6 @@ function weightedMovingAverage(data, length) {
     if(data.length < length)
         return undefined;
     
-    console.log("computing weighted average");
-
     var averages = new Array(data.length - length + 1);
 
     var denominator = length * (length + 1) / 2;
@@ -47,9 +46,7 @@ function weightedMovingAverage(data, length) {
         numerator += (i+1) * data[i][1];
         total += data[i][1];
     }
-    console.log("COMPUTING AVERAGE");
     averages[0] = [data[i+1][0], numerator / denominator];
-    console.log("Averages: ", averages);
 
     // now the weighted
     var previousTotal, previousNumerator;
@@ -59,12 +56,9 @@ function weightedMovingAverage(data, length) {
 
         total = previousTotal + data[i + length - 1][1] - data[i - 1][1];
         numerator = previousNumerator + length * data[i + length - 1][1] - previousTotal;
-        console.log("SETTING TO: ", [data[i - 1][0], numerator / denominator]);
         averages[i] = [data[i - 1][0], numerator / denominator];
     }
     
-    console.log(averages);
-
     return averages;
 }
 
@@ -96,12 +90,31 @@ function inspect(object, depth) {
 	}
 }
 
+function createSeries(array) {
+	var maparray = [];
+	for(var i = 0; i < array.length; i++) {
+		maparray[i] = {x:array[i][0], y:array[i][1]};
+	}
+	
+	return maparray;
+}
+
 function AxPlot(id) {
 	// pull the original data
 	dojo.xhrGet({
 			url: '/monitorconsole.axmonitorwidget/'+id,
 			handleAs: 'json',
 			load: function(data) {
+				if(data.length < 5) {
+					dojo.destroy(id+"_spin");
+					dojo.create(
+							'div', 
+							{className: 'status', innerHTML: 'not enough data'}, 
+							dojo.getById(id+"_plot")
+					);
+					return;
+				}
+				
 				var chart = new dojox.charting.Chart2D(id + "_plot");
 				
 				var i;
@@ -117,7 +130,8 @@ function AxPlot(id) {
 				chart.addPlot('smooth', {
 					type: 'Lines',
 					lines: true,
-			        tension: 5
+			        tension: 5,
+			        shadows: 0
 				});
 			    chart.addPlot('raw', {type: 'Lines', lines: true, vAxis: 'y'});
 				
@@ -127,12 +141,18 @@ function AxPlot(id) {
 			    	max: 5000,
 			    	minorTicks: false,
 			    	majorTicks: [0, 1000],
-			    	labels: [{value: 0, text:'0s'}, {value: 1000, text:'1s'}]
+			    	labels: [{value: 0, text:'0s'}, {value: 1000, text:'1s'}, {value: 2000, text:'2s'}, {value: 3000, text:'3s'}, {value: 4000, text:'4s'}, {value: 5000}]
 			    });
 			    
-				chart.addSeries('time.smooth', smoother, {plot:'smooth'});
-				chart.addSeries('time.raw', responseSize, {plot:'raw'});
+			    chart.addAxis("x", {
+			    	
+			    });
+			    
+			    chart.addSeries('time.smooth', createSeries(smoother), {plot: 'smooth'});
+			    chart.addSeries('time.raw', createSeries(responseSize), {plot: 'raw'});
 				
+			    new dojox.charting.action2d.Tooltip(chart, "smooth", {scale: 3});
+			    
 				chart.render();
 				
 				// remove spinner
