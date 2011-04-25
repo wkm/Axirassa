ccd5388fe9c4cb606d2abc29f982ef265061e318
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-04-24 21:10:37 -0600

    Trying to work some magic with class loaders (to automatically unpack libraries on calls to findLibrary)
    --------------------------------------------------
    ACTIVE - BugzID:192: Ensure the interface can start from within overlord
    https://zanoccio.fogbugz.com/default.asp?192

--- src/main/java/zanoccio/javakit/LibraryUnpackingClassLoader.java
+++ src/main/java/zanoccio/javakit/LibraryUnpackingClassLoader.java
@@ -1,19 +1,70 @@
 
 package zanoccio.javakit;
 
+import java.net.URL;
+import java.net.URLClassLoader;
+
 /**
- * A ClassLoader which replaces requests to {@link java.lang.System} with
- * requests to {@link LibraryUnpackingSystem}. Quite naughty.
+ * A ClassLoader which provides it's own #findLibrary routine (by extracting
+ * libraries from JAR files)
  * 
  * @author wiktor
  */
 public class LibraryUnpackingClassLoader extends ClassLoader {
+
+	private static class FindClassClassLoader extends ClassLoader {
+		public FindClassClassLoader (ClassLoader parent) {
+			super(parent);
+		}
+
+
+		@Override
+		public Class<?> findClass (String name) throws ClassNotFoundException {
+			return super.findClass(name);
+		}
+	}
+
+
+
+
+
+	private static class ChildUrlClassLoader extends URLClassLoader {
+		private FindClassClassLoader realParent;
+
+
+		public ChildUrlClassLoader (URL[] urls, FindClassClassLoader realParent) {
+			super(urls, null);
+			this.realParent = realParent;
+		}
+
+
+		@Override
+		public Class<?> findClass (String name) throws ClassNotFoundException {
+			System.out.println("Finding class: " + name);
+			try {
+				return super.findClass(name);
+			} catch (ClassNotFoundException e) {
+				return realParent.loadClass(name);
+			}
+		}
+	}
+
+
+	private ChildUrlClassLoader childClassLoader;
+
+
+	public LibraryUnpackingClassLoader () {
+		super(Thread.currentThread().getContextClassLoader());
+		childClassLoader = new ChildUrlClassLoader(new URL[] {}, new FindClassClassLoader(this.getParent()));
+	}
+
+
 	@Override
-	protected synchronized Class loadClass (String className, boolean resolve) throws ClassNotFoundException {
-		if (className.equals("java.lang.System")) {
-			System.out.println("INTERCEPTING, REPLACING WITH LibraryUnpackingSystem CLASS");
-			return super.loadClass("zanoccio.javakit.LibraryUnpackingSystem", resolve);
-		} else
-			return super.loadClass(className, resolve);
+	protected Class<?> loadClass (String name, boolean resolve) throws ClassNotFoundException {
+		try {
+			return childClassLoader.findClass(name);
+		} catch (ClassNotFoundException e) {
+			return super.loadClass(name, resolve);
+		}
 	}
 }
