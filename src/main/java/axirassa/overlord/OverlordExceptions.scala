d4381c427d61ca7262d473b19a2090a0ddd56a68
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-04-26 21:40:39 -0600

    Migrate exceptions to scala; consolidate into one file (beautiful)
    --------------------------------------------------
    ACTIVE - BugzID:201: Migrate overlord to scala
    https://zanoccio.fogbugz.com/default.asp?201

---
+++ src/main/java/axirassa/overlord/OverlordExceptions.scala
@@ -0,0 +1,51 @@
+
+package axirasa.overlord.exceptions
+
+import axirassa.overlord.ExecutionTarget
+import axirassa.overlord.ExecutionGroup
+import org.w3c.dom.Document
+
+abstract class OverlordException(msg : String, cause : Exception = null) extends Exception(msg, cause)
+
+abstract class OverlordError(cause : Throwable) extends Error(cause)
+
+//
+// ERRORS
+//
+class ExceptionInMonitorError(e : Exception) extends OverlordError(e)
+
+//
+// EXCEPTIONS
+//
+class DuplicateGroupException(group : ExecutionGroup, doc : Document)
+    extends OverlordException("A group named "+group.getCanonicalName+" is already defined in "+doc.getBaseURI)
+
+class DuplicateTargetException(target : ExecutionTarget, doc : Document)
+    extends OverlordException("A target with name "+target.getCanonicalName+" already exists in "+doc.getBaseURI)
+
+class EmptyExecutionGroupException(name : String, doc : Document)
+    extends OverlordException(name+" execution group is empty in "+doc.getBaseURI)
+
+class InvalidOverlordNameExcepion(name : String, pattern : String)
+    extends OverlordException("Names must match pattern "+pattern+" name given: "+name)
+
+class NoExecutionTargetsException(doc : Document)
+    extends OverlordException("No execution targets in configuration at: "+doc.getBaseURI)
+
+class NoGroupsException(doc : Document)
+    extends OverlordException("No execution groups found in "+doc.getBaseURI)
+
+class NoOverlordConfigurationException(filename : String)
+    extends OverlordException("Could not locate configuration file "+filename)
+
+class OverlordParsingException(doc : Document, exception : Exception)
+    extends OverlordException("Error while parsing "+doc, exception)
+
+class OverlordTargetClassNotFoundException(target : String, doc : Document, e : ClassNotFoundException)
+    extends OverlordException(target+" target class could not be found in "+doc.getBaseURI, e)
+
+class UnknownExecutionTargetException(name : String, doc : Document)
+    extends OverlordException("No target named "+name+" found in "+doc.getBaseURI)
+
+class UnsupportedPlatformException(platform : String)
+    extends OverlordException(platform+" is not a supported platform for Overlord")
