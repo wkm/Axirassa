fb2187474922beaceb1ac795d6d01b2ebbe44e4a
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-04-26 20:09:33 -0600

    Trigger pages ------------------------------------------------- ACTIVE - BugzID:200: Migrate tapestry pages to scala https://zanoccio.fogbugz.com/default.asp?200

---
+++ src/main/java/axirassa/webapp/pages/trigger/CreateTrigger.scala
@@ -0,0 +1,16 @@
+package axirassa.webapp.pages.trigger
+
+import org.apache.tapestry5.annotations.Property
+import org.apache.shiro.authz.annotation.RequiresUser
+
+@RequiresUser
+class CreateTrigger {
+	@Property
+	var latency : String = _
+
+	@Property
+	var responseTime : String = _
+
+	@Property
+	var responseSize : String = _
+}
