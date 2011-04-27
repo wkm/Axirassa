3b57c6fdabd245bf4fb146625cc24938bb807d65
wiktor@zanoccio.com <wiktor@zanoccio.com>
2011-04-26 18:05:03 -0600

    Rewrite Messaging as a scala object

---
+++ src/main/java/axirassa/config/Messaging.scala
@@ -0,0 +1,15 @@
+package axirassa.config
+
+object Messaging {
+    val PINGER_REQUEST_QUEUE = "Pinger.Request";
+    val PINGER_RESPONSE_QUEUE = "Pinger.Response";
+
+    val NOTIFY_EMAIL_REQUEST = "Notify.Email.Request";
+    val NOTIFY_EMAIL_RESPONSE = "Notify.Email.Response";
+
+    val NOTIFY_SMS_REQUEST = "Notify.Sms.Request";
+    val NOTIFY_SMS_RESPONSE = "Notify.Sms.Response";
+
+    val NOTIFY_VOICE_REQUEST = "Notify.Voice.Request";
+    val NOTIFY_VOICE_RESPONSE = "Notify.Voice.Response";
+} 
