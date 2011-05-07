be528074c67982713c57cb086204bf8463398b33
wiktor <devnull@localhost>
2011-05-07 02:42:10 -0600

    Fix up credentials tests

--- src/test/java/test/axirassa/webapp/services/TestUserCredentialsMatcher.java
+++ src/test/java/test/axirassa/webapp/services/TestUserCredentialsMatcher.java
@@ -8,13 +8,11 @@ import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.hibernate.Query;
 import org.hibernate.Session;
-import org.junit.Before;
 import org.junit.Test;
 
 import axirassa.dao.UserDAO;
 import axirassa.dao.UserEmailAddressDAO;
 import axirassa.model.UserEmailAddressEntity;
-import axirassa.model.UserEntity;
 import axirassa.model.flows.CreateUserFlow;
 import axirassa.util.test.AbstractIntegrationTest;
 import axirassa.webapp.services.UserAuthenticationInfo;
@@ -35,50 +33,26 @@ public class TestUserCredentialsMatcher extends AbstractIntegrationTest {
 	private CreateUserFlow createUser;
 
 
-	@Before
+	@Test
 	public void createUsers() {
 		database.beginTransaction();
+		createUser.setEmail("who1@foo.com");
+		createUser.setPassword("password");
+		createUser.execute();
 
-		UserEntity user = new UserEntity();
-		UserEmailAddressEntity email = new UserEmailAddressEntity();
-
-		user.createPassword("password");
-		database.save(user);
-
-		email.setEmail("who1@foo.com");
-		email.setPrimaryEmail(true);
-		email.setUser(user);
-		database.save(email);
-
-		// create another user
-		user = new UserEntity();
-		user.createPassword("password");
-		database.save(user);
-
-		email = new UserEmailAddressEntity();
-		email.setEmail("who2@foo.com");
-		email.setPrimaryEmail(true);
-		email.setUser(user);
-		database.save(email);
-
+		createUser.setEmail("who2@foo.com");
+		createUser.setPassword("password");
+		createUser.execute();
 		database.getTransaction().commit();
 
 		Query q = database.createQuery("from UserEntity");
 		assertEquals(2, q.list().size());
-	}
-
-
-	@Test
-	public void testMatcher() {
-		database.beginTransaction();
 
 		UserCredentialsMatcher matcher = new UserCredentialsMatcher(database);
 		UserEmailAddressEntity email = addressDAO.getPrimaryEmail(userDAO.getUserByEmail("who1@foo.com"));
 		UserAuthenticationInfo authinfo = UserAuthenticationInfo.createInfoFromEntity(email);
-
 		UsernamePasswordToken token1 = new UsernamePasswordToken("who1@foo.com", "password");
 		assertTrue(matcher.doCredentialsMatch(token1, authinfo));
-
-		database.getTransaction().commit();
 	}
+
 }
