a16afbad6fe43fc94d809466bab9090d6e863046
wiktor <devnull@localhost>
2011-05-08 18:00:32 -0600

    Add dependency for tapestry-xpath; create a trivial page test.

--- src/test/java/test/axirassa/webapp/pages/TestRegisterPage.java
+++ src/test/java/test/axirassa/webapp/pages/TestRegisterPage.java
@@ -1,16 +1,29 @@
 
 package test.axirassa.webapp.pages;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import org.apache.tapestry5.dom.Document;
+import org.apache.tapestry5.dom.Node;
+import org.jaxen.JaxenException;
 import org.junit.Test;
 
 import axirassa.util.test.TapestryPageTest;
 
+import com.formos.tapestry.xpath.TapestryXPath;
+
 public class TestRegisterPage extends TapestryPageTest {
 	@Test
-	public void registerPage() {
+	public void registerPage() throws JaxenException {
 		System.out.println("TESTER: " + tester);
-		Document page = tester.renderPage("/user/register");
-		System.out.println(page);
+		Document page = tester.renderPage("user/register");
+
+		List<Node> textNodes = TapestryXPath.xpath("//input[@type='text']").selectNodes(page);
+		assertEquals(3, textNodes.size());
+
+		List<Node> passwordNodes = TapestryXPath.xpath("//input[@type='password']").selectNodes(page);
+		assertEquals(2, passwordNodes.size());
 	}
 }
