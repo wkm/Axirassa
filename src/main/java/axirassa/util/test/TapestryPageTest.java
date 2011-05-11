13cbeb64172a0e8c9246462175eff5b5e50e4246
wiktor <devnull@localhost>
2011-05-11 17:11:04 -0600

    The DAO module is extraneous since it's included through the flows module

--- src/main/java/axirassa/util/test/TapestryPageTest.java
+++ src/main/java/axirassa/util/test/TapestryPageTest.java
@@ -9,7 +9,6 @@ import org.apache.shiro.realm.Realm;
 import org.tynamo.security.services.TapestryRealmSecurityManager;
 
 import axirassa.ioc.AxirassaSecurityModule;
-import axirassa.ioc.DAOModule;
 import axirassa.ioc.ExternalServicesMockingModule;
 import axirassa.ioc.FlowsModule;
 import axirassa.ioc.LoggingModule;
@@ -19,20 +18,18 @@ import com.formos.tapestry.testify.core.TapestryTester;
 import com.formos.tapestry.testify.junit4.TapestryTest;
 
 public class TapestryPageTest extends TapestryTest {
-	private static final TapestryTester SHARED_TESTER = new TapestryTester("axirassa.webapp", DAOModule.class,
-	        FlowsModule.class, MessagingModule.class, LoggingModule.class, ExternalServicesMockingModule.class,
+	private static final TapestryTester SHARED_TESTER = new TapestryTester("axirassa.webapp", FlowsModule.class,
+	        MessagingModule.class, LoggingModule.class, ExternalServicesMockingModule.class,
 	        AxirassaSecurityModule.class);
 
 
 	public TapestryPageTest() {
 		super(SHARED_TESTER);
-
 		setSecurityManager();
 	}
 
 
 	private void setSecurityManager() {
-		System.out.println("NO SECURITY MANAGER; CREATING IT");
 		CustomSecurityManagerFactory factory = SHARED_TESTER.autobuild(CustomSecurityManagerFactory.class);
 
 		Collection<Realm> realms = new ArrayList<Realm>();
