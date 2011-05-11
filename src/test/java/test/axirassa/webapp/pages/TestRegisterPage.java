83856ba1d7538d1e972522670dc61a971842774a
wiktor <devnull@localhost>
2011-05-11 17:24:05 -0600

    Simple test registration page

--- src/test/java/test/axirassa/webapp/pages/TestRegisterPage.java
+++ src/test/java/test/axirassa/webapp/pages/TestRegisterPage.java
@@ -2,6 +2,8 @@
 package test.axirassa.webapp.pages;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
@@ -10,9 +12,11 @@ import org.apache.tapestry5.dom.Element;
 import org.apache.tapestry5.dom.Node;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.hibernate.Session;
-import org.jaxen.JaxenException;
 import org.junit.Test;
 
+import axirassa.dao.UserDAO;
+import axirassa.model.UserEntity;
+import axirassa.model.flows.CreateUserFlow;
 import axirassa.util.test.MapUtility;
 import axirassa.util.test.TapestryPageTest;
 
@@ -22,14 +26,19 @@ public class TestRegisterPage extends TapestryPageTest {
 	@Inject
 	Session session;
 
+	@Inject
+	UserDAO userDAO;
+
+	@Inject
+	CreateUserFlow createUser;
+
 
 	@Test
-	public void registerPage() throws JaxenException {
-		System.out.println("TESTER: " + tester);
+	public void registerPage() throws Exception {
 		Document page = tester.renderPage("user/register");
 
 		List<Node> textNodes = TapestryXPath.xpath("//input[@type='text']").selectNodes(page);
-		assertEquals(3, textNodes.size());
+		assertEquals(3, textNodes.size()); // an extra node for feedback
 
 		List<Node> passwordNodes = TapestryXPath.xpath("//input[@type='password']").selectNodes(page);
 		assertEquals(2, passwordNodes.size());
@@ -42,7 +51,15 @@ public class TestRegisterPage extends TapestryPageTest {
 
 		// ensure we have no errors
 		List<Element> errorNodes = TapestryXPath.xpath("//*[@class='t-error']").selectElements(resultPage);
+		System.out.println(resultPage);
 		assertEquals(0, errorNodes.size());
+
+		// did we save this user?
+		UserEntity user = userDAO.getUserByEmail("who@foo.com");
+		assertNotNull(user);
+
+		// password should match
+		assertTrue(user.matchPassword("123"));
 
 		System.out.println(resultPage);
 	}
