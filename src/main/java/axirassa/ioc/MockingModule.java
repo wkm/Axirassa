fdcdb897d5df8ac0ff4e18ac0d07bb479d4d7cca
wiktor <devnull@localhost>
2011-05-16 21:42:32 -0600

    Mock the HibernateSessionSource and the HibernateSessionManager to prevent hibernate dependencies from connecting to a local DB when running what are supposed to be unit tests.

--- src/main/java/axirassa/ioc/MockingModule.java
+++ src/main/java/axirassa/ioc/MockingModule.java
@@ -3,6 +3,8 @@ package axirassa.ioc;
 
 import static org.mockito.Mockito.mock;
 
+import org.apache.tapestry5.hibernate.HibernateSessionManager;
+import org.apache.tapestry5.hibernate.HibernateSessionSource;
 import org.apache.tapestry5.ioc.MappedConfiguration;
 import org.apache.tapestry5.ioc.annotations.SubModule;
 import org.hibernate.Session;
@@ -15,6 +17,8 @@ import org.hibernate.Session;
 @SubModule({ ExternalServicesMockingModule.class, MockPageRenderLinkSourceModule.class })
 public class MockingModule {
 	public static void contributeServiceOverride(MappedConfiguration<Class<?>, Object> configuration) {
+		configuration.add(HibernateSessionSource.class, mock(HibernateSessionSource.class));
+		configuration.add(HibernateSessionManager.class, mock(HibernateSessionManager.class));
 		configuration.add(Session.class, mock(Session.class));
 	}
 }
