b2febde65784d805d0fb37ccab9bc788a1ca57f8
wiktor <devnull@localhost>
2011-05-23 14:50:21 -0600

    Inject the WebSecurityManager instead of just the entityRealm.

--- src/main/java/axirassa/util/test/CustomSecurityManagerFactory.java
+++ src/main/java/axirassa/util/test/CustomSecurityManagerFactory.java
@@ -1,14 +1,15 @@
+
 package axirassa.util.test;
 
 import lombok.Getter;
 
-import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.web.mgt.WebSecurityManager;
 import org.apache.tapestry5.ioc.annotations.Inject;
 
 public class CustomSecurityManagerFactory {
 	@Inject
 	@Getter
-	private AuthorizingRealm entityRealm;
+	private WebSecurityManager webSecurityManager;
 
 
 	public CustomSecurityManagerFactory() {
