e771bde4cf50b6ef6a30dbef79a38662676c66ab
wiktor <devnull@localhost>
2011-05-23 14:50:04 -0600

    Robust test of the login page

--- src/test/java/test/axirassa/webapp/pages/TestLoginPage.java
+++ src/test/java/test/axirassa/webapp/pages/TestLoginPage.java
@@ -1,22 +1,48 @@
 
 package test.axirassa.webapp.pages;
 
+import java.util.LinkedHashMap;
+
 import org.apache.tapestry5.dom.Document;
 import org.apache.tapestry5.dom.Element;
-import org.jaxen.JaxenException;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.junit.Test;
 
-import axirassa.util.test.MapUtility;
+import axirassa.model.flows.CreateUserFlow;
 import axirassa.util.test.TapestryPageTest;
 
 import com.formos.tapestry.xpath.TapestryXPath;
 
 public class TestLoginPage extends TapestryPageTest {
+	@Inject
+	CreateUserFlow createUser;
+
+	@Inject
+	Session session;
+
+
 	@Test
-	public void testLogin() throws JaxenException {
+	public void testLogin() throws Exception {
+		// create a simple user
+		createUser.setEmail("who@foo.com");
+		createUser.setPassword("123");
+		createUser.execute();
+
+		Query users = session.createQuery("from UserEntity");
+		System.out.println("USER LIST: " + users.list());
+
 		Document page = tester.renderPage("user/login");
 		Element registerButton = TapestryXPath.xpath("//input[@value='Login']").selectSingleElement(page);
-		tester.clickSubmit(registerButton, new MapUtility().p("txtfield", "who@foo.com").p("txtfield_0", "123")
-		        .getValueMap());
+		Document result = tester.clickSubmit(registerButton, new LinkedHashMap<String, String>() {
+			{
+				put("txtfield", "who@foo.com");
+				put("txtfield_0", "123");
+			}
+		});
+
+		// System.out.println(result);
+		ensureNoErrors(result);
 	}
 }
