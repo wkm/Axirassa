4c5c100edd1761e98afcb87d2fe60898a5bd47bd
wiktor <devnull@localhost>
2011-05-24 17:33:05 -0600

    Declare ResetPasswordFlow

--- src/main/java/axirassa/ioc/FlowsModule.java
+++ src/main/java/axirassa/ioc/FlowsModule.java
@@ -11,12 +11,15 @@ import axirassa.model.flows.AddEmailAddressFlow;
 import axirassa.model.flows.AddEmailAddressFlowImpl;
 import axirassa.model.flows.CreateUserFlow;
 import axirassa.model.flows.CreateUserFlowImpl;
+import axirassa.model.flows.ResetPasswordFlow;
+import axirassa.model.flows.ResetPasswordFlowImpl;
 
 @SubModule({ DAOModule.class })
 public class FlowsModule {
 	public static void bind(ServiceBinder binder) {
 		binder.bind(CreateUserFlow.class, CreateUserFlowImpl.class);
 		binder.bind(AddEmailAddressFlow.class, AddEmailAddressFlowImpl.class);
+		binder.bind(ResetPasswordFlow.class, ResetPasswordFlowImpl.class);
 	}
 
 
