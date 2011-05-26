e30a15901e8c39d00891e1de8c7e64a730c6ea6a
wiktor <devnull@localhost>
2011-05-25 19:15:02 -0600

    Fix up AxTextField and AxPasswordField to use correct clientId()s for validation and better HTML structure

--- src/main/java/axirassa/webapp/components/AxPasswordField.java
+++ src/main/java/axirassa/webapp/components/AxPasswordField.java
@@ -1,34 +1,59 @@
 
 package axirassa.webapp.components;
 
+import org.apache.tapestry5.ComponentResources;
+import org.apache.tapestry5.Field;
 import org.apache.tapestry5.annotations.Component;
+import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.corelib.components.PasswordField;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.javascript.JavaScriptSupport;
 
-public class AxPasswordField extends AxAbstractTextInput {
-	@Component(publishParameters = "annotationProvider,clientId,disabled,nulls,translate,validate,value")
-	private PasswordField txtfield;
+public class AxPasswordField implements Field {
+	@Inject
+	private ComponentResources componentResources;
+
+	@Inject
+	private JavaScriptSupport jsSupport;
+
+	@Component(publishParameters = "label,annotationProvider,clientId,disabled,nulls,translate,value,id,validate")
+	@Property
+	private PasswordField textfield;
+
+	private String clientId;
+
+
+	public void setupRender() {
+		clientId = jsSupport.allocateClientId(componentResources);
+	}
 
 
 	@Override
 	public String getControlName() {
-		return txtfield.getControlName();
+		return textfield.getControlName();
 	}
 
 
 	@Override
 	public boolean isDisabled() {
-		return txtfield.isDisabled();
+		return textfield.isDisabled();
 	}
 
 
 	@Override
 	public boolean isRequired() {
-		return txtfield.isRequired();
+		return textfield.isRequired();
 	}
 
 
 	@Override
 	public String getClientId() {
-		return txtfield.getClientId();
+		return clientId;
+	}
+
+
+	@Override
+	public String getLabel() {
+		return textfield.getLabel();
 	}
 }
--- src/main/java/axirassa/webapp/components/AxTextField.java
+++ src/main/java/axirassa/webapp/components/AxTextField.java
@@ -1,47 +1,59 @@
 
 package axirassa.webapp.components;
 
+import org.apache.tapestry5.ComponentResources;
+import org.apache.tapestry5.Field;
 import org.apache.tapestry5.annotations.Component;
-import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.corelib.components.TextField;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.javascript.JavaScriptSupport;
 
-public class AxTextField extends AxAbstractTextInput {
-	@Component(
-	        publishParameters = "annotationProvider,clientId,disabled,nulls,translate,value",
-	        parameters = { "validate=inherit:validate" })
-	private TextField txtfield;
+public class AxTextField implements Field {
+	@Inject
+	private ComponentResources componentResources;
 
-	@Parameter(defaultPrefix = "validate")
+	@Inject
+	private JavaScriptSupport jsSupport;
+
+	@Component(publishParameters = "label,annotationProvider,clientId,disabled,nulls,translate,value,id,validate")
 	@Property
-	private String validate;
+	private TextField textfield;
+
+	private String clientId;
 
 
-	public TextField getTextField() {
-		return txtfield;
+	public void setupRender() {
+		clientId = jsSupport.allocateClientId(componentResources);
 	}
 
 
 	@Override
 	public String getControlName() {
-		return txtfield.getControlName();
+		return textfield.getControlName();
 	}
 
 
 	@Override
 	public boolean isDisabled() {
-		return txtfield.isDisabled();
+		return textfield.isDisabled();
 	}
 
 
 	@Override
 	public boolean isRequired() {
-		return txtfield.isRequired();
+		return textfield.isRequired();
 	}
 
 
 	@Override
 	public String getClientId() {
-		return txtfield.getClientId();
+		return clientId;
+	}
+
+
+	@Override
+	public String getLabel() {
+		return textfield.getLabel();
 	}
 }
