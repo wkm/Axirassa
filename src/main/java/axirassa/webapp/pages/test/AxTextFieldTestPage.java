6619ac6e79134249c3b4364dd45def9b67968824
wiktor <devnull@localhost>
2011-05-26 23:43:15 -0600

    remove @Property annoations by using empty string literals

--- src/main/java/axirassa/webapp/pages/test/AxTextFieldTestPage.java
+++ src/main/java/axirassa/webapp/pages/test/AxTextFieldTestPage.java
@@ -1,15 +1,7 @@
 
 package axirassa.webapp.pages.test;
 
-import org.apache.tapestry5.annotations.Property;
 
 public class AxTextFieldTestPage {
-	@Property
-	private String text;
-
-	@Property
-	private String text1;
-
-	@Property
-	private String text2;
+	// empty
 }
