907a80a8bdf50d26e223d7a7704fa43679bf8afb
wiktor <devnull@localhost>
2011-05-31 21:16:09 -0600

    Move most of the compression grunt work from JSONResponse into CompressableTextResponse for reuse opportunities

--- src/main/java/axirassa/util/JSONResponse.java
+++ src/main/java/axirassa/util/JSONResponse.java
@@ -1,16 +1,10 @@
 
 package axirassa.util;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
-import java.util.zip.GZIPOutputStream;
 
-import org.apache.tapestry5.StreamResponse;
 import org.apache.tapestry5.json.JSONCollection;
-import org.apache.tapestry5.services.Response;
 
 /**
  * Based on:
@@ -20,67 +14,22 @@ import org.apache.tapestry5.services.Response;
  * 
  * @author wiktor
  */
-public class JSONResponse implements StreamResponse {
+public class JSONResponse extends CompressableTextResponse {
 
 	private static final String CHARSET = "UTF-8";
-	private static final int MIN_DATA_SIZE = 512;
-
-	private byte[] data;
-	private byte[] dataForSending;
-	private boolean isCompressable;
 
 
-	public JSONResponse (JSONCollection json) throws IOException {
+	public JSONResponse(JSONCollection json) throws IOException {
 		try {
-			data = json.toCompactString().getBytes(CHARSET);
-
-			if (data.length >= MIN_DATA_SIZE)
-				isCompressable = true;
-			else
-				isCompressable = false;
-
-			if (!isCompressable) {
-				dataForSending = data;
-				return;
-			}
-
-			compressData();
-			data = null;
-
+			setResponseData(json.toCompactString().getBytes(CHARSET));
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace(System.err);
 		}
 	}
 
 
-	private void compressData () throws IOException {
-		ByteArrayOutputStream outStream = new ByteArrayOutputStream(data.length);
-		GZIPOutputStream gzip = new GZIPOutputStream(outStream);
-		gzip.write(data);
-		gzip.close();
-
-		dataForSending = outStream.toByteArray();
-	}
-
-
 	@Override
-	public String getContentType () {
+	public String getContentType() {
 		return "application/json; charset=" + CHARSET;
-	}
-
-
-	@Override
-	public InputStream getStream () throws IOException {
-		return new ByteArrayInputStream(dataForSending);
-	}
-
-
-	@Override
-	public void prepareResponse (Response response) {
-		System.out.println("PREPARED RESPONSE W/ GZIP LENGTH: " + dataForSending.length);
-		if (isCompressable) {
-			response.setHeader("Content-Encoding", "gzip");
-			response.setIntHeader("Content-Length", dataForSending.length);
-		}
 	}
 }
