e83b4272ed79346c3921e0e765bc6ac826758133
wiktor <wiktor@zanoccio.com>
2011-06-23 13:34:23 -0600

    Create a variant of the monitor widget which uses flash charts.

--- src/main/java/axirassa/webapp/components/AxFlashMonitorWidget.java
+++ src/main/java/axirassa/webapp/components/AxFlashMonitorWidget.java
@@ -3,18 +3,30 @@ package axirassa.webapp.components;
 
 import org.apache.tapestry5.ComponentResources;
 import org.apache.tapestry5.Link;
+import org.apache.tapestry5.annotations.Environmental;
 import org.apache.tapestry5.annotations.Import;
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.PageRenderLinkSource;
+import org.apache.tapestry5.services.javascript.JavaScriptSupport;
 
+import axirassa.dao.PingerDAO;
+import axirassa.model.PingerEntity;
 import axirassa.webapp.pages.monitor.DataMonitor;
 
 @Import(library = { "context:/js/amcharts/flash/swfobject.js" })
 public class AxFlashMonitorWidget {
 	@Inject
 	private PageRenderLinkSource linkSource;
+
+	@Inject
+	private ComponentResources resources;
+
+	@Inject
+	private PingerDAO pingerDAO;
+
+	private PingerEntity pinger;
 
 	@Property
 	@Parameter(required = true, defaultPrefix = "literal")
@@ -39,12 +51,24 @@ public class AxFlashMonitorWidget {
 	@Property
 	private String dataUrl;
 
-	@Inject
-	private ComponentResources resources;
+	@Property
+	private String clientId;
+
+	@Property
+	private String pingerName;
+
+	@Environmental
+	private JavaScriptSupport jssupport;
 
 
 	private void setupRender() {
 		Link link = linkSource.createPageRenderLinkWithContext(DataMonitor.class);
 		dataUrl = link.toAbsoluteURI() + ":csv/" + pingerId;
+
+		clientId = jssupport.allocateClientId(resources);
+
+		// temporary hack to fix
+		pinger = pingerDAO.findPingerById(pingerId);
+		pingerName = pinger.getUrl().replace("http://", "");
 	}
 }
