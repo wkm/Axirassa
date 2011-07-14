1b180f906e10540451c7b737863df7e6cdd0ba33
wiktor <devnull@localhost>
2011-07-13 21:50:15 -0600

    Extract a BackoffStrategy interface; adjust the strategy interface to track failures and successes (so we can reset the failure counter)

--- src/main/java/axirassa/messaging/util/ExponentialBackoffStrategy.java
+++ src/main/java/axirassa/messaging/util/ExponentialBackoffStrategy.java
@@ -7,13 +7,15 @@ package axirassa.messaging.util;
  * 
  * @author wiktor
  */
-public class ExponentialBackoffStrategy {
+public class ExponentialBackoffStrategy implements BackoffStrategy {
+	private int successTicks = 0;
+	private int failureTicks = -1;
 
-	private int ticks = -1;
 	private final int backoffActivationIndex;
 	private final int minimumBackoff;
 	private final int maximumBackoff;
 	private final int increment;
+	private final int successBackoffReset;
 
 
 	/**
@@ -23,32 +25,48 @@ public class ExponentialBackoffStrategy {
 	 *            the maximum backoff that can be computed, set to 0 for no
 	 *            limit
 	 * @param backoffActivationIndex
-	 *            the number of of ticks before a backoff is computed. set to 0
+	 *            the number of of failureTicks before a backoff is computed. set to 0
 	 *            to immediately backoff
 	 * @param increment
 	 *            the basic amount to increment, once backoff is activated it
-	 *            will follow the pattern
+	 *            will follow the pattern X, 2X, 4X, 8X, 16X, etc.
+	 * @param successBackoffReset
+	 *            the amount of successful failureTicks it takes to reset the backoff
 	 */
-	public ExponentialBackoffStrategy(int minimumBackoff, int maximumBackoff, int backoffActivationIndex, int increment) {
+	public ExponentialBackoffStrategy(int minimumBackoff, int maximumBackoff, int backoffActivationIndex,
+	        int increment, int successBackoffReset) {
 		this.minimumBackoff = minimumBackoff;
 		this.maximumBackoff = maximumBackoff;
 		this.backoffActivationIndex = backoffActivationIndex;
 		this.increment = increment;
+		this.successBackoffReset = successBackoffReset;
 	}
 	
-	public void tickBackoff() {
-		ticks++;
+	@Override
+    public void tickFailure() {
+		failureTicks++;
+		successTicks = 0;
 	}
 
 
-	public long computeBackoffDelay() {
+	@Override
+	public void tickSuccess() {
+		successTicks++;
+
+		if (successTicks > successBackoffReset) {
+			failureTicks = -1;
+			successTicks = 0;
+		}
+	}
+
+
+	@Override
+    public long computeBackoffDelay() {
 		long computedBackoff = 0;
-		int effectiveTicks = ticks - backoffActivationIndex;
+		int effectiveTicks = failureTicks - backoffActivationIndex;
 		
 		if(effectiveTicks < 0)
 			return 0;
-		
-		System.out.println("EFFECTIVE TICKS: " + effectiveTicks);
 
 		computedBackoff = increment * (long) Math.pow(2, effectiveTicks);
 
