aa3fa811fcb7f0d40074e836eefdc438ce0a01d2
wiktor <devnull@localhost>
2011-07-14 02:53:34 -0600

    escalate to throwing Throwables instead of Exception

--- src/main/java/zanoccio/javakit/lambda/Function1.java
+++ src/main/java/zanoccio/javakit/lambda/Function1.java
@@ -1,5 +1,5 @@
 package zanoccio.javakit.lambda;
 
 public interface Function1<R, A> {
-	public R call(A p1) throws Exception;
+	public R call(A p1) throws Throwable;
 }
--- src/main/java/zanoccio/javakit/lambda/Function2.java
+++ src/main/java/zanoccio/javakit/lambda/Function2.java
@@ -1,5 +1,5 @@
 package zanoccio.javakit.lambda;
 
 public interface Function2<R, A, B> {
-	public R call(A p1, B p2) throws Exception;
+	public R call(A p1, B p2) throws Throwable;
 }
