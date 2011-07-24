151b280e5d431362101302bd0928e7eea023260c
wiktor <devnull@localhost>
2011-07-24 17:44:44 -0600

    Rewrite CommonBackoffStrategies.EXPONENTIAL_BACKOFF_MESSAGING as a static method instead of this weird cloning business

--- src/main/java/axirassa/messaging/util/CommonBackoffStrategies.java
+++ src/main/java/axirassa/messaging/util/CommonBackoffStrategies.java
@@ -1,6 +1,8 @@
+
 package axirassa.messaging.util;
 
 public class CommonBackoffStrategies {
-	public final static ExponentialBackoffStrategy EXPONENTIAL_BACKOFF_MESSAGING = new ExponentialBackoffStrategy(0,
-	        60000, 3, 1000, 60);
+	public final static ExponentialBackoffStrategy EXPONENTIAL_BACKOFF_MESSAGING() {
+		return new ExponentialBackoffStrategy(0, 60000, 3, 1000, 60);
+	}
 }
