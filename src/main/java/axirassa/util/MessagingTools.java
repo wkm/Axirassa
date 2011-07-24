fb77ba79c07324941a40bcfbc61312427b4e9f04
wiktor <devnull@localhost>
2011-07-24 17:17:54 -0600

    Add #fromMessageBytes(targetClass, message) to handle message object conversion and validation

--- src/main/java/axirassa/util/MessagingTools.java
+++ src/main/java/axirassa/util/MessagingTools.java
@@ -11,6 +11,8 @@ import org.hornetq.api.core.client.HornetQClient;
 import org.hornetq.api.core.client.ServerLocator;
 import org.hornetq.core.remoting.impl.netty.NettyConnectorFactory;
 
+import axirassa.services.exceptions.InvalidMessageClassException;
+
 public class MessagingTools {
 	public static ClientSession getEmbeddedSession() throws Exception {
 		TransportConfiguration configuration = new TransportConfiguration(NettyConnectorFactory.class.getName());
@@ -30,5 +32,13 @@ public class MessagingTools {
 
 	public static Object fromMessageBytes(ClientMessage message) throws IOException, ClassNotFoundException {
 		return AutoSerializingObject.fromBytes(bodyBytes(message));
+	}
+	
+	public static <T> T fromMessageBytes(Class<? extends T> messageClass, ClientMessage message) throws IOException, ClassNotFoundException, InvalidMessageClassException {
+		Object rawobject = fromMessageBytes(message);
+		if(messageClass.isInstance(rawobject))
+			return (T) rawobject;
+		else
+			throw new InvalidMessageClassException(messageClass, rawobject);
 	}
 }
