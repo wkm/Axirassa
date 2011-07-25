98f8b9cd62d88e4606b4e4080964467324f6a699
wiktor <devnull@localhost>
2011-07-25 14:54:47 -0600

    Create a thread for managing creation and destruction of pinging threads

--- src/main/java/axirassa/services/pinger/PingerServiceRateMonitoringThread.java
+++ src/main/java/axirassa/services/pinger/PingerServiceRateMonitoringThread.java
@@ -1,10 +1,55 @@
+
 package axirassa.services.pinger;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.hornetq.api.core.client.ClientConsumer;
+
+import zanoccio.javakit.lambda.Function1;
+import axirassa.messaging.util.CommonBackoffStrategies;
+import axirassa.messaging.util.InfiniteLoopExceptionSurvivor;
+
+@Slf4j
 public class PingerServiceRateMonitoringThread implements Runnable {
+	private ConcurrentLinkedQueue<PingerServiceCoordinationMessage> pingRequestQueue;
+	private ClientConsumer throttlingQueueConsumer;
+
+	private int threadsStarted = 0;
+	private int threadsStopped = 0;
+
+
+	public PingerServiceRateMonitoringThread(ClientConsumer throttlingQueueConsumer,
+	        ConcurrentLinkedQueue<PingerServiceCoordinationMessage> pingRequestQueue) {
+		this.throttlingQueueConsumer = throttlingQueueConsumer;
+		this.pingRequestQueue = pingRequestQueue;
+	}
+
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		InfiniteLoopExceptionSurvivor executor = new InfiniteLoopExceptionSurvivor(
+		        CommonBackoffStrategies.EXPONENTIAL_BACKOFF_MESSAGING(), 
+		        new Callable<Object>() {
+					@Override
+                    public Object call() throws Exception {
+	                    // TODO Auto-generated method stub
+	                    return null;
+                    }
+		        }, new Function1<Boolean, Throwable>() {
+					@Override
+                    public Boolean call(Throwable t) throws Throwable {
+	                    t.printStackTrace(System.err);
+	                    return null;
+                    }
+		        });
 
+		try {
+			executor.execute();
+		} catch (Throwable t) {
+			log.error("Exception: ", t);
+		}
 	}
 }
