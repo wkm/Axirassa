bb5bb4864951b0c70224edeb61729c67faac8fb0
wiktor <devnull@localhost>
2011-07-25 14:52:52 -0600

    Create a simple bridge class that allows an Axirassa service to be used as a Runnable

---
+++ src/main/java/axirassa/services/util/ServiceRunnableBridge.java
@@ -0,0 +1,25 @@
+
+package axirassa.services.util;
+
+import lombok.extern.slf4j.Slf4j;
+import axirassa.services.Service;
+
+@Slf4j
+public class ServiceRunnableBridge implements Runnable {
+	private Service service;
+
+
+	public ServiceRunnableBridge(Service service) {
+		this.service = service;
+	}
+
+
+	@Override
+	public void run() {
+		try {
+			service.execute();
+		} catch (Throwable t) {
+			log.error("Exception ", t);
+		}
+	}
+}
