99da35f772883a831a63317a3a89e639ac2a8836
wiktor <devnull@localhost>
2011-07-25 14:53:54 -0600

    Move bandwidth thread allocator and bandwidth measurer instantiation and configuration into the PTS runner as opposed to the PTS service.

--- src/main/java/axirassa/services/pinger/PingerThrottlingService.java
+++ src/main/java/axirassa/services/pinger/PingerThrottlingService.java
@@ -1,19 +1,28 @@
 
 package axirassa.services.pinger;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 import lombok.extern.slf4j.Slf4j;
 
+import org.hornetq.api.core.HornetQException;
 import org.hornetq.api.core.client.ClientConsumer;
 import org.hornetq.api.core.client.ClientMessage;
 import org.hornetq.api.core.client.ClientProducer;
 import org.hornetq.api.core.client.ClientSession;
 
+import zanoccio.javakit.ListUtilities;
 import zanoccio.javakit.lambda.Function1;
 import axirassa.config.Messaging;
+import axirassa.messaging.AbstractPingerThrottlingMessage;
+import axirassa.messaging.PingerDownThrottleMessage;
+import axirassa.messaging.PingerUpThrottlingMessage;
 import axirassa.messaging.util.CommonBackoffStrategies;
 import axirassa.messaging.util.InfiniteLoopExceptionSurvivor;
 import axirassa.model.HttpStatisticsEntity;
@@ -22,12 +31,16 @@ import axirassa.util.MessagingTools;
 import axirassa.util.MessagingTopic;
 
 public class PingerThrottlingService implements Service {
-	private final BandwidthMeasurer measurer = new BandwidthMeasurer(60000);
+	private final BandwidthMeasurer measurer;
+	private final BandwidthThreadAllocator threadAllocator;
 	private final ClientSession messaging;
 
 
-	public PingerThrottlingService(ClientSession session) {
+	public PingerThrottlingService(ClientSession session, BandwidthMeasurer bandwidthMeasurer,
+	        BandwidthThreadAllocator threadAllocator) {
 		this.messaging = session;
+		this.measurer = bandwidthMeasurer;
+		this.threadAllocator = threadAllocator;
 	}
 
 
@@ -41,33 +54,73 @@ public class PingerThrottlingService implements Service {
 		new Thread(new PingerBandwidthMeasurementPopulatorThread(pingerResponseConsumer, measurer)).run();
 
 		ScheduledThreadPoolExecutor scheduledExecutor = new ScheduledThreadPoolExecutor(1);
-		scheduledExecutor.scheduleAtFixedRate(new PingerThreadCountAdjusterThread(throttlingProducer, measurer), 0,
-		                                      100, TimeUnit.MILLISECONDS);
+		Runnable adjusterThread = new PingerThreadCountAdjusterThread(messaging, throttlingProducer, measurer,
+		        threadAllocator);
+		scheduledExecutor.scheduleAtFixedRate(adjusterThread, 0, 100, TimeUnit.MILLISECONDS);
 	}
 }
 
 @Slf4j
 class PingerThreadCountAdjusterThread implements Runnable {
 	private final int currentThreadCount = 0;
+
+	private final ClientSession messaging;
 	private final ClientProducer throttlingProducer;
 	private final BandwidthMeasurer measurer;
+	private final BandwidthThreadAllocator bandwidthAllocator;
 
 
-	PingerThreadCountAdjusterThread(ClientProducer throttlingProducer, BandwidthMeasurer measurer) {
+	PingerThreadCountAdjusterThread(ClientSession messaging, ClientProducer throttlingProducer,
+	        BandwidthMeasurer measurer, BandwidthThreadAllocator bandwidthAllocator) {
+		this.messaging = messaging;
 		this.measurer = measurer;
 		this.throttlingProducer = throttlingProducer;
+		this.bandwidthAllocator = bandwidthAllocator;
 	}
 
 
 	@Override
 	public void run() {
-		printCurrentStatus();
+		try {
+			bandwidthAllocator.addBandwidthMeasurement(measurer.currentRate(1000, System.currentTimeMillis()));
+			int threadDelta = bandwidthAllocator.getTargetThreadCountDelta();
+
+			printCurrentStatus(threadDelta);
+
+			for (AbstractPingerThrottlingMessage messageBody : createMessages(threadDelta)) {
+				ClientMessage message = messaging.createMessage(true);
+				message.getBodyBuffer().writeBytes(messageBody.toBytes());
+				throttlingProducer.send(message);
+			}
+		} catch (HornetQException e) {
+			e.printStackTrace(System.err);
+		} catch (IOException e) {
+			e.printStackTrace(System.err);
+		}
 	}
 
 
-	private void printCurrentStatus() {
-		// TODO Auto-generated method stub
+	private PingerDownThrottleMessage downThrottleMessage = new PingerDownThrottleMessage();
+	private PingerUpThrottlingMessage upThrottlingMessage = new PingerUpThrottlingMessage();
 
+
+	private List<AbstractPingerThrottlingMessage> createMessages(int threadDelta) {
+		ArrayList<AbstractPingerThrottlingMessage> list = new ArrayList<AbstractPingerThrottlingMessage>(threadDelta);
+
+		if (threadDelta == 0)
+			return Collections.EMPTY_LIST;
+
+		if (threadDelta < 0)
+			return ListUtilities.padRight(list, -threadDelta, downThrottleMessage);
+		else
+			return ListUtilities.padRight(list, threadDelta, upThrottlingMessage);
+	}
+
+
+	private void printCurrentStatus(int threadDelta) {
+		System.out.printf("THREADS: %4d DELTA: %5d BANDWIDTH: %8d  QUEUE LENGTH: %5d\n",
+		                  bandwidthAllocator.getCurrentThreads(), threadDelta,
+		                  measurer.currentRate(1000, System.currentTimeMillis()));
 	}
 }
 
--- src/main/java/axirassa/services/runners/PingerThrottlingServiceRunner.java
+++ src/main/java/axirassa/services/runners/PingerThrottlingServiceRunner.java
@@ -1,15 +1,22 @@
+
 package axirassa.services.runners;
 
 import org.hornetq.api.core.client.ClientSession;
 
 import axirassa.services.Service;
+import axirassa.services.pinger.BandwidthMeasurer;
+import axirassa.services.pinger.BandwidthThreadAllocator;
 import axirassa.services.pinger.PingerThrottlingService;
 import axirassa.util.MessagingTools;
 
 public class PingerThrottlingServiceRunner {
 	public static void main(String[] args) throws Throwable {
+		// 50 threads base; 2000 max; we want 10megabytes/second
+		BandwidthThreadAllocator threadAllocator = new BandwidthThreadAllocator(50, 2000, 50, 10000000);
+		BandwidthMeasurer bandwidthMeasurer = new BandwidthMeasurer(60000);
+
 		ClientSession session = MessagingTools.getEmbeddedSession();
-		Service service = new PingerThrottlingService(session);
+		Service service = new PingerThrottlingService(session, bandwidthMeasurer, threadAllocator);
 		service.execute();
 	}
 }
