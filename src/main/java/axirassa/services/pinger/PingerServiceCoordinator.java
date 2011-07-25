dc5281fe0993872baf7cceea0a1c106520b124a9
wiktor <devnull@localhost>
2011-07-25 16:10:29 -0600

    Get rid of direct dependencies on ConcurrentLinkedQueue and create a gentle wrapper in PingerServiceCoordinator; this is needed mainly so we can place kill-requests directly at the front of the queue.

---
+++ src/main/java/axirassa/services/pinger/PingerServiceCoordinator.java
@@ -0,0 +1,31 @@
+
+package axirassa.services.pinger;
+
+import java.util.LinkedList;
+
+public class PingerServiceCoordinator {
+	private LinkedList<PingerServiceCoordinationMessage> queue = new LinkedList<PingerServiceCoordinationMessage>();
+
+
+	synchronized public void append(PingerServiceCoordinationMessage message) {
+		queue.addLast(message);
+	}
+
+
+	synchronized public void prepend(PingerServiceCoordinationMessage message) {
+		queue.addFirst(message);
+	}
+	
+	synchronized public int size() {
+		return queue.size();
+	}
+	
+	synchronized public PingerServiceCoordinationMessage peek() {
+		return queue.getFirst();
+	}
+	
+	synchronized public PingerServiceCoordinationMessage pollFirst() {
+		return queue.pollFirst();
+	}
+
+}
