ed16b313f9a4a9fc7e950873bb4dc9a7a3abce7b
wiktor <devnull@localhost>
2011-07-24 21:31:22 -0600

    Create messages for up and down throttling

---
+++ src/main/java/axirassa/messaging/PingerDownThrottleMessage.java
@@ -0,0 +1,8 @@
+
+package axirassa.messaging;
+
+import axirassa.util.AutoSerializingObject;
+
+public class PingerDownThrottleMessage extends AutoSerializingObject {
+    private static final long serialVersionUID = -6157564025030484033L;
+}
---
+++ src/main/java/axirassa/messaging/PingerUpThrottlingMessage.java
@@ -0,0 +1,7 @@
+package axirassa.messaging;
+
+import axirassa.util.AutoSerializingObject;
+
+public class PingerUpThrottlingMessage extends AutoSerializingObject{
+    private static final long serialVersionUID = -6217756203298813402L;
+}
