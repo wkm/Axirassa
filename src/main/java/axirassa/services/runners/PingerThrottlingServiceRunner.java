c6c16ef7cd65d431cdf7f8820395f5b5d12e269c
wiktor <devnull@localhost>
2011-07-24 21:49:29 -0600

    Fix up throttling service runner

--- src/main/java/axirassa/services/runners/PingerThrottlingServiceRunner.java
+++ src/main/java/axirassa/services/runners/PingerThrottlingServiceRunner.java
@@ -3,12 +3,13 @@ package axirassa.services.runners;
 import org.hornetq.api.core.client.ClientSession;
 
 import axirassa.services.Service;
+import axirassa.services.pinger.PingerThrottlingService;
 import axirassa.util.MessagingTools;
 
 public class PingerThrottlingServiceRunner {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Throwable {
 		ClientSession session = MessagingTools.getEmbeddedSession();
-		Service service = new PingerThrottlingService();
+		Service service = new PingerThrottlingService(session);
 		service.execute();
 	}
 }
