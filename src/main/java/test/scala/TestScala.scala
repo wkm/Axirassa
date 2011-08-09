8ce22b549f525ea1f68090cec6b9a7c8a8406eaf
wiktor <devnull@localhost>
2011-08-09 01:33:46 -0600

    Create a simple test case for scala

---
+++ src/main/java/test/scala/TestScala.scala
@@ -0,0 +1,17 @@
+package test.scala
+
+import org.junit.Test
+import org.junit.Assert._
+
+object TestScala extends App {
+  override def main(args: Array[String]) {
+    println("Hello World")
+  }
+}
+
+class TestScala {
+  @Test
+  def test() {
+    assertEquals("hi", "hi")
+  }
+}
